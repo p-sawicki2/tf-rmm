969da9cd9426763ac72e9a53c024bae9a93844a7
Javier Almansa Sobrino <javier.almansasobrino@arm.com>
2022-07-05 19:05:14 +0100

    feat(unittests): add a unit test framework.
    This patch adds all the infrastructure needed to run unit tests for
    RMM, including a new variant for platform host, called build_test.
    
    To build and run the tests:
    
    cmake -DRMM_CONFIG=host_defcfg -DHOST_VARIANT=host_test -S ${RMM_SOURCE_DIR} -B ${RMM_BUILD_DIR}
    cmake --build ${RMM_BUILD_DIR}
    cmake --build ${RMM_BUILD_DIR} -- run-unittests
    
    Signed-off-by: Javier Almansa Sobrino <javier.almansasobrino@arm.com>
    Change-Id: If16686e111d91c563f8e7281d4ee7ca2864125ae

---
+++ cmake/BuildCppUTest.cmake
@@ -0,0 +1,20 @@
+#
+# SPDX-License-Identifier: BSD-3-Clause
+# SPDX-FileCopyrightText: Copyright TF-RMM Contributors.
+#
+
+add_subdirectory("${CMAKE_SOURCE_DIR}/ext/cpputest"
+                 "${CMAKE_BINARY_DIR}/ext/cpputest")
+
+unset(cpputest_CXX_FLAGS)
+
+if(CMAKE_VERBOSE_MAKEFILE)
+    list(APPEND cpputest_CXX_FLAGS -D "CMAKE_VERBOSE_MAKEFILE=1")
+endif()
+
+string(APPEND cpputest_CXX_FLAGS "-Wno-c++98-compat-pedantic")
+
+target_compile_options(CppUTest
+    PRIVATE
+        ${cpputest_CXX_FLAGS}
+)
