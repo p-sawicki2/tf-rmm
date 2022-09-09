3ac8299b2914c06df53d4feb71fc7774ed7d5d24
Javier Almansa Sobrino <javier.almansasobrino@arm.com>
2022-09-09 16:01:17 +0100

    feat(tests): Add support for coverage tests
    This patch enables support for coverity on the
    unit tests by using the gcovr tool.
    
    The coverity report is stored in ${RMM_BUILD_DIR}/Testing/coverity
    
    Signed-off-by: Javier Almansa Sobrino <javier.almansasobrino@arm.com>
    Change-Id: I5b6a2f3ebf13cbc036c0d64f422ea18f337dd8f5

--- toolchains/fake_host/llvm.cmake
+++ toolchains/fake_host/llvm.cmake
@@ -17,7 +17,16 @@ set(CMAKE_ASM_COMPILER ${CMAKE_C_COMPILER})
 foreach(language IN ITEMS ASM C CXX)
     string(APPEND CMAKE_${language}_FLAGS_INIT "-Wno-unknown-warning-option ")
     string(APPEND CMAKE_${language}_FLAGS_INIT "-Wno-unused-function ")
+    string(APPEND CMAKE_${language}_FLAGS_INIT "-O0 ")
+    string(APPEND CMAKE_${language}_FLAGS_INIT "-fcoverage-mapping ")
+    string(APPEND CMAKE_${language}_FLAGS_INIT "-ftest-coverage ")
+    string(APPEND CMAKE_${language}_FLAGS_INIT "-fprofile-arcs ")
+    string(APPEND CMAKE_${language}_FLAGS_INIT "-fprofile-instr-generate ")
 endforeach()
 
 string(APPEND CMAKE_EXE_LINKER_FLAGS_INIT "-Wl,--build-id=none ")
 string(APPEND CMAKE_EXE_LINKER_FLAGS_INIT "-fuse-ld=lld ")
+
+# Setup the right coverage tool if using llvm
+set(GCOV_EXECUTABLE --gcov-executable "llvm-cov gcov"
+    CACHE INTERNAL "GCOV_EXECUTABLE")
