347fd491f64bb06d584cb6afc750e8a1fea541df
Javier Almansa Sobrino <javier.almansasobrino@arm.com>
2022-09-09 16:01:17 +0100

    feat(tests): Add support for coverage tests
    This patch enables support for coverity on the
    unit tests by using the gcovr tool.
    
    The coverity report is stored in ${RMM_BUILD_DIR}/Testing/coverity
    
    Signed-off-by: Javier Almansa Sobrino <javier.almansasobrino@arm.com>
    Change-Id: I5b6a2f3ebf13cbc036c0d64f422ea18f337dd8f5

--- toolchains/fake_host/gnu.cmake
+++ toolchains/fake_host/gnu.cmake
@@ -22,4 +22,15 @@ find_program(CMAKE_CXX_COMPILER
 
 set(CMAKE_ASM_COMPILER ${CMAKE_C_COMPILER})
 
-string(APPEND CMAKE_EXE_LINKER_FLAGS_INIT "-Wl,--build-id=none ")
+#
+# Flags needed to enable coverage testing
+#
+
+if (COVERAGE_ENABLED STREQUAL "ON")
+    foreach(language in ITEMS C CXX)
+        string(APPEND CMAKE_${language}_FLAGS_INIT "--coverage ")
+        string(APPEND CMAKE_${language}_FLAGS_INIT "-O0 ")
+    endforeach()
+endif()
+
+string(APPEND CMAKE_EXE_LINKER_FLAGS_INIT "-Wl,--build-id=none")
