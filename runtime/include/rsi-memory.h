7ac9a58be8dcf8c7af00e630316fe10e33896b27
Arunachalam Ganapathy <arunachalam.ganapathy@arm.com>
2022-11-03 13:56:18 +0000

    fix(rsi): handle destroyed state s2tte in RSI_IPA_STATE_GET ABI
    In the RSI_IPA_STATE_GET ABI implementation, if the s2tte has state
    HIPAS=DESTROYED for the given IPA, then emulate a Data Abort and exit
    to the Host.
    
    Signed-off-by: Arunachalam Ganapathy <arunachalam.ganapathy@arm.com>
    Change-Id: If909fcac74748052f98f3f4d190ceb3cc6c2fbc8

--- runtime/include/rsi-memory.h
+++ runtime/include/rsi-memory.h
@@ -6,14 +6,25 @@
 #ifndef	RSI_MEMORY_H
 #define	RSI_MEMORY_H
 
-#include <smc-rsi.h>
+#include <rsi-walk.h>
+#include <smc.h>
 
 struct rec;
 struct rmi_rec_exit;
 
+struct rsi_ripas_get_res {
+	/* Result of RTT walk performed by RSI command */
+	struct rsi_walk_result walk_result;
+
+	/*
+	 * If @walk_result.abort is false, @smc_res contains GPR values to be
+	 * returned to the Realm.
+	 */
+	struct smc_result smc_res;
+};
+
 bool handle_rsi_ipa_state_set(struct rec *rec, struct rmi_rec_exit *rec_exit);
 
-rsi_status_t handle_rsi_ipa_state_get(struct rec *rec, unsigned long ipa,
-				      enum ripas *ripas);
+struct rsi_ripas_get_res handle_rsi_ipa_state_get(struct rec *rec);
 
 #endif /* RSI_MEMORY_H */
