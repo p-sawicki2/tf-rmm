4331f25f6af4e971b3d4b67d7e283072eab58fc4
Javier Almansa Sobrino <javier.almansasobrino@arm.com>
2022-12-08 14:53:14 +0000

    feat(unittests): Add support to handle assertions on unittests
    Change-Id: Ifabca247ad094b92214d7e1fe2698ce15361e379
    Signed-off-by: Javier Almansa Sobrino <javier.almansasobrino@arm.com>

--- plat/host/host_test/include/test_helpers.h
+++ plat/host/host_test/include/test_helpers.h
@@ -7,6 +7,72 @@
 #define TEST_HELPERS_H
 
 /*
+ * Defines whether we expect or not an assert to fail.
+ *
+ * Arguments:
+ *	- expected: True if we are expecting assertion to fail.
+ *		    False if we do not expect an assertion to fail.
+ *	- check: If NULL and expected is True, it will consider any
+ *		 assertion failure as valid. If not NULL, the assertion check
+ *		 that failed must match the one in check to be considered
+ *		 valid.
+ *
+ * If, during the execution of the test, an assertion happens and it is not
+ * expected or the check fails, the current unittest with FAIL.
+ *
+ * Conversely, if an expected assertion happens, the test will FINISH and
+ * will be marked as PASSED.
+ *
+ * NOTE: After an assertion failure, regardless of whether it was expected or
+ * not, the next assertion failure will be considered unexpected and
+ * therefore the test will fail. If we expect another assertion to fail, then
+ * this API must be called again to indicate that. It is recommended to setup
+ * the expected assert() behavior during TEST_SETUP().
+ *
+ * NOTE2: If an assertion failure was expected but did not happen, it will
+ * not cause the test to FAIL and therefore the test will continue. If no
+ * other condition causes a test failure and the call that was expected to
+ * cause the assert failure manages to return, the unittest must fail
+ * inmediately.
+ * See test_helper_fail_if_not_assertion()
+ */
+void test_helper_expect_assert_with_check(bool expected, char *check);
+
+/*
+ * Defines whether we expect or not an assert to fail.
+ *
+ * Arguments:
+ *	- expected: True if we are expecting assertion to fail.
+ *		    False if we do not expect an assertion to fail.
+ *
+ * If, during the execution of the test, an assertion happens and it is not
+ * expected or the check fails, the current unittest with FAIL.
+ *
+ * Conversely, if an expected assertion happens, the test will FINISH and
+ * will be marked as PASSED.
+ *
+ * NOTE: After an assertion failure, regardless of whether it was expected or
+ * not, the next assertion failure will be considered unexpected and
+ * therefore the test will fail. If we expect another assertion to fail, then
+ * this API must be called again to indicate that. It is recommended to setup
+ * the expected assert() behavior during TEST_SETUP().
+ *
+ * NOTE2: If an assertion failure was expected but did not happen, it will
+ * not cause the test to FAIL and therefore the test will continue. If no
+ * other condition causes a test failure and the call that was expected to
+ * cause the assert failure manages to return, the unittest must fail
+ * inmediately.
+ * See test_helper_fail_if_not_assertion()
+ */
+void test_helper_expect_assert(bool expected);
+
+/*
+ * Call this function to fail a test if an expected assert fail did not happen.
+ * This is preferred than fail with FAIL_TEST().
+ */
+void test_helper_fail_if_no_assertion(void);
+
+/*
  * Helper function to fully initialize RMM.
  *
  * Args
