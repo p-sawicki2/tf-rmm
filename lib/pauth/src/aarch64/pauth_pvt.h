b4244aa15b46eb542c4540ddfa413da93415783f
Arvind Ram Prakash <arvind.ramprakash@arm.com>
2022-12-15 12:16:36 -0600

    feat(PAuth): add support for RMM and Realms
    * This patch adds support for use of Pointer Authentication (PAuth) feature
      in Realms and enable PAuth in RMM (R-EL2).
    * The EL3 is expected to context switch the PAuth key registers and hence RMM
      does not need to save the NS key values.
    * The PAuth registers corresponding to REC is restored before entering a Realm
      and the same is saved and RMM kays IA keys are restored on Realm exit.
    
    Signed-off-by: Arvind Ram Prakash <arvind.ramprakash@arm.com>
    Signed-off-by: Shruti Gupta <shruti.gupta@arm.com>
    Change-Id: I9b19d16478f660f5b2e037069a7e5f9a782f06fb

---
+++ lib/pauth/src/aarch64/pauth_pvt.h
@@ -0,0 +1,11 @@
+/*
+ * SPDX-License-Identifier: BSD-3-Clause
+ * SPDX-FileCopyrightText: Copyright TF-RMM Contributors
+ */
+
+#ifndef PAUTH_PVT_H
+#define PAUTH_PVT_H
+
+#define PAUTH_NONE_ATTR __attribute__((target("branch-protection=none")))
+
+#endif /* PAUTH_PVT_H */
