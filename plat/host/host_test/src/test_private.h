885b6fd6d083a3908ec7c64915b4cba3187da5dd
Javier Almansa Sobrino <javier.almansasobrino@arm.com>
2022-12-28 10:36:25 +0000

    feat(unittests): add support to handle panic() on unittests
    This patch also moves the panic() implementation to the platform
    layer
    
    Signed-off-by: Javier Almansa Sobrino <javier.almansasobrino@arm.com>
    Change-Id: Ifdfde5db35727862f3d7ef5587036b9b721cf0be

---
+++ plat/host/host_test/src/test_private.h
@@ -0,0 +1,13 @@
+/*
+ * SPDX-License-Identifier: BSD-3-Clause
+ * SPDX-FileCopyrightText: Copyright TF-RMM Contributors.
+ */
+
+#ifndef TEST_PRIVATE_H
+#define TEST_PRIVATE_H
+
+#include <utils_def.h>
+
+__dead2 void test_private_panic(void);
+
+#endif /* TEST_PRIVATE_H */
