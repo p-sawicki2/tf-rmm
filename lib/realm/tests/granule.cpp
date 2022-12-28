80e727add1bca351dd5c039252587a84ef3dda18
Javier Almansa Sobrino <javier.almansasobrino@arm.com>
2022-12-28 10:36:25 +0000

    feat(unittests): add support to handle panic() on unittests
    This patch also moves the panic() implementation to the platform
    layer
    
    Signed-off-by: Javier Almansa Sobrino <javier.almansasobrino@arm.com>
    Change-Id: Ifdfde5db35727862f3d7ef5587036b9b721cf0be

--- lib/realm/tests/granule.cpp
+++ lib/realm/tests/granule.cpp
@@ -122,6 +122,7 @@ TEST_GROUP(granule) {
 		}
 
 		test_helper_expect_assert(false);
+		test_helper_expect_panic(false);
 	}
 
 	TEST_TEARDOWN()
---
+++ plat/fvp/include/platform.h
@@ -0,0 +1,13 @@
+/*
+ * SPDX-License-Identifier: BSD-3-Clause
+ * SPDX-FileCopyrightText: Copyright TF-RMM Contributors.
+ */
+
+#ifndef PLATFORM_H
+#define PLATFORM_H
+
+#define panic()				\
+	do {				\
+	} while (true)
+
+#endif /* PLATFORM_H */
