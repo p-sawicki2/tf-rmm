811883f6aac8dbc9cbfea7d0b10748f57ec27248
Javier Almansa Sobrino <javier.almansasobrino@arm.com>
2022-12-28 10:36:25 +0000

    feat(unittests): add support to handle panic() on unittests
    This patch also moves the panic() implementation to the platform
    layer
    
    Signed-off-by: Javier Almansa Sobrino <javier.almansasobrino@arm.com>
    Change-Id: Ifdfde5db35727862f3d7ef5587036b9b721cf0be

--- plat/host/host_test/src/test_private.h
+++ plat/host/host_test/src/test_private.h
@@ -7,10 +7,13 @@
 #define TEST_PRIVATE_H
 
 #include <test_helpers.h>
+#include <utils_def.h>
 
 /*
  * Return a callback for a give callback ID
  */
 uintptr_t get_cb(enum cb_ids id);
+
+__dead2 void test_private_panic(void);
 
 #endif /* TEST_PRIVATE_H */
