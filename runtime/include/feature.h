6b846b763b36c1ec4d2350dbeb968461185666bf
AlexeiFedorov <Alexei.Fedorov@arm.com>
2023-01-17 17:04:14 +0000

    feat(rmm): report PMU not supported
    This patch reports for PMU not being supported by RMM
    and adds relevant definitions for Feature Register 0
    in accordance with Beta 0 RMM Specification.
    It also masks PMUVer field when ID_AA64DFR0_EL1
    register is read by Realm.
    ID_AA64DFR0_EL1 register mask and set values are
    modified to report minimum Debug features supported
    by ARM architecture.
    
    Signed-off-by: AlexeiFedorov <Alexei.Fedorov@arm.com>
    Change-Id: I9974dac476df270f309f6bb30d3485ba0af5986e

--- runtime/include/feature.h
+++ runtime/include/feature.h
@@ -8,6 +8,16 @@
 
 #include <arch.h>
 
+/* RmiFeature enumerations */
+#define RMI_NOT_SUPPORTED				UL(0)
+#define RMI_SUPPORTED					UL(1)
+
+#define RMM_FEATURE_REGISTER_0_PMU_EN_SHIFT		UL(22)
+#define RMM_FEATURE_REGISTER_0_PMU_EN_WIDTH		UL(1)
+
+#define RMM_FEATURE_REGISTER_0_PMU_NUM_CTRS_SHIFT	UL(23)
+#define RMM_FEATURE_REGISTER_0_PMU_NUM_CTRS_WIDTH	UL(5)
+
 bool validate_feature_register(unsigned long index, unsigned long value);
 
 #endif /* FEATURE_H */
