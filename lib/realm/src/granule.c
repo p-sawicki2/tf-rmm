8bd2158eaea4ce62e37190656d96591650bf5951
Soby Mathew <soby.mathew@arm.com>
2023-01-18 10:57:18 +0100

    Revert "fix(granules): granule_addr() did not detect a misaligned address"
    This reverts commit aecad126cf873639b66bb99d84267bc08f4dce2b.
    
    Reason for revert: This causes runtime failure which needs to be checked.
    
    Change-Id: Ie75f099c1423140f7e4828603ecd92692fc2452d

--- lib/realm/src/granule.c
+++ lib/realm/src/granule.c
@@ -29,7 +29,6 @@ unsigned long granule_addr(const struct granule *g)
 	unsigned long idx;
 
 	assert(g != NULL);
-	assert(ALIGNED((void *)g, sizeof(struct granule)));
 	assert(g >= &granules[0]);
 
 	idx = g - &granules[0];
