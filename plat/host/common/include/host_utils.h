5eb5955869e24f60504c8870f0947bd47caaa11c
Javier Almansa Sobrino <javier.almansasobrino@arm.com>
2023-03-02 12:26:39 +0000

    feat(host_utils): add support for sysregs snapshots management
    This patch adds functionality to the host_utils library to take
    and restore snapshots of the system registers status on the
    fake_host architecture so the sysregs can be kept between runs
    of different test groups.
    
    Signed-off-by: Javier Almansa Sobrino <javier.almansasobrino@arm.com>
    Change-Id: Ifb705380f079308576e7f59b475be6984447f045

--- plat/host/common/include/host_utils.h
+++ plat/host/common/include/host_utils.h
@@ -6,6 +6,7 @@
 #ifndef HOST_UTILS_H
 #define HOST_UTILS_H
 
+#include <stddef.h>
 #include <types.h>
 
 /***********************************************************************
@@ -113,9 +114,41 @@ int host_util_set_sysreg_cb(char *name, rd_cb_t rd_cb, wr_cb_t wr_cb,
 int host_util_set_default_sysreg_cb(char *name, u_register_t init);
 
 /*
- * Clear the list of sysreg callbacks.
+ * Save the sysreg state across all PEs in the system along with registered
+ * callbacks. This function is meant to be invoked at a point wherein the
+ * system is initialized properly and can restored for later test iterations.
+ *
+ * Arguments:
+ *	snapshot - Pointer to an array of struct sysreg_data where the
+ *		   snapshot will be stored.
+ *	len	 - Number of items on the snapshot array. Must be at least
+ *		   SYSREG_MAX_CBS.
+ *
+ * Returns:
+ *	0 on success or a negative error code otherwise.
  */
-void host_util_reset_all_sysreg_cb(void);
+int host_util_take_sysreg_snapshot(struct sysreg_data *snapshot, size_t len);
+
+/*
+ * Restore a saved sysreg state and associated callbacks. The state is already
+ * assumed to be saved prior to calling this API.
+ *
+ * Arguments:
+ *	snapshot - Pointer to an array of struct sysreg_data where the
+ *		   snapshot are be stored.
+ *	len	 - Number of items on the snapshot array. Must be at least
+ *		   SYSREG_MAX_CBS.
+ *
+ * Returns:
+ *	0 on success or a negative error code otherwise.
+ */
+int host_util_restore_sysreg_snapshot(struct sysreg_data *snapshot,
+				      size_t len);
+
+/*
+ * Zero all sysreg values and unregister all sysreg callbacks.
+ */
+void host_util_zero_sysregs_and_cbs(void);
 
 /*
  * Return the configured address for the granule base.
