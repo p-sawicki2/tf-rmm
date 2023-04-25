6c07e6db2d789b2328b9fd0d48c5bef8d9db571e
Soby Mathew <soby.mathew@arm.com>
2023-04-25 11:21:32 +0100

    feat(fake_host):  Enable generating profile data
    This patch adds necessary CFlags for generating the profiling
    data when binary is executed.
    
    Signed-off-by: Soby Mathew <soby.mathew@arm.com>
    Change-Id: Id988e7fcb18048cc4d34ced3a339e73b116be786

---
+++ toolchains/fake_host/common_fake_host.cmake
@@ -0,0 +1,13 @@
+#
+# SPDX-License-Identifier: BSD-3-Clause
+# SPDX-FileCopyrightText: Copyright TF-RMM Contributors.
+#
+
+include_guard()
+
+include(${CMAKE_CURRENT_LIST_DIR}/../common.cmake)
+
+foreach(language IN ITEMS ASM C CXX)
+    string(APPEND CMAKE_${language}_FLAGS_INIT "-fno-omit-frame-pointer -pg ")
+    string(APPEND CMAKE_${language}_FLAGS_INIT "-gdwarf-4 ")
+endforeach()
