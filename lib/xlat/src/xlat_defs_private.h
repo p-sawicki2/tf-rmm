112463eddcb61f22ca72faa87869cd3069f98e9c
Javier Almansa Sobrino <javier.almansasobrino@arm.com>
2023-04-27 17:42:58 +0100

    feat(xlat): add support for FEAT_LPA2 to the stage 1 xlat library
    The xlat library is able to work with and without FEAT_LPA2 supported
    by the architecture.
    
    This patch also hardcodes the shareability attribute for all
    memory as ISH.
    
    In order to maintain simplicity and make the review process
    easier, this patch does not update the unittests nor any of the
    host variants, hence they will not work on the current patch.
    
    Unittests as well as the host variants will be fixed on a separate
    patch.
    
    Signed-off-by: Javier Almansa Sobrino <javier.almansasobrino@arm.com>
    Change-Id: I5501371c602f358a650b005eda2c2f9d076a4b33

--- lib/xlat/src/xlat_defs_private.h
+++ lib/xlat/src/xlat_defs_private.h
@@ -10,21 +10,9 @@
 #define XLAT_DEFS_PRIVATE_H
 
 #include <arch.h>
+#include <arch_features.h>
 #include <utils_def.h>
 #include <xlat_defs.h>
-
-/* Miscellaneous MMU related constants */
-#define NUM_2MB_IN_GB		(UL(1) << 9)
-#define NUM_4K_IN_2MB		(UL(1) << 9)
-#define NUM_GB_IN_4GB		(UL(1) << 2)
-
-#define TWO_MB_SHIFT		UL(21)
-#define ONE_GB_SHIFT		UL(30)
-#define FOUR_KB_SHIFT		UL(12)
-
-#define ONE_GB_INDEX(x)		((x) >> ONE_GB_SHIFT)
-#define TWO_MB_INDEX(x)		((x) >> TWO_MB_SHIFT)
-#define FOUR_KB_INDEX(x)	((x) >> FOUR_KB_SHIFT)
 
 /*
  * A block descriptor points to a region of memory bigger than the granule size
@@ -32,23 +20,20 @@
  */
 /* Definition of a valid descriptor mask */
 #define VALID_DESC		UL(0x1)
-#define BLOCK_DESC		VALID_DESC /* Table levels 0-2 */
+#define BLOCK_DESC		VALID_DESC /* Table levels [0, 2] */
 /* A table descriptor points to the next level of translation table. */
-#define TABLE_DESC		(UL(0x2) | VALID_DESC) /* Table levels 0-2 */
+#define TABLE_DESC		(UL(0x2) | VALID_DESC) /* Table levels [-1, 2] */
 /* Definition of an invalid descriptor */
 #define INVALID_DESC		UL(0x0)
 /*
- * A page descriptor points to a page, i.e. a memory region whose size is the
+ * A TTE points to a page, i.e. a memory region whose size is the
  * translation granule size (e.g. 4KB).
  */
 #define PAGE_DESC		(UL(0x2) | VALID_DESC) /* Table level 3 */
 
 #define DESC_MASK		UL(0x3)
 
-#define FIRST_LEVEL_DESC_N	ONE_GB_SHIFT
-#define SECOND_LEVEL_DESC_N	TWO_MB_SHIFT
-#define THIRD_LEVEL_DESC_N	FOUR_KB_SHIFT
-
+/* Upper attributes on a TTE */
 #define XN			(ULL(1) << 2)
 #define UXN			(ULL(1) << 2)
 #define PXN			(ULL(1) << 1)
@@ -59,25 +44,14 @@
 #define UPPER_ATTRS_MASK	MASK(UPPER_ATTRS)
 #define UPPER_ATTRS(x)		(INPLACE(UPPER_ATTRS, x) & (UPPER_ATTRS_MASK))
 
-#define NON_GLOBAL		(UL(1) << 9)
-#define ACCESS_FLAG		(UL(1) << 8)
-#define NSH			(UL(0x0) << 6)
-#define OSH			(UL(0x2) << 6)
-#define ISH			(UL(0x3) << 6)
-
 /* Guarded Page bit */
 #define GP			(ULL(1) << 50)
 
-/* Table address field on a table descriptor given 4KB granularity. */
-#define TABLE_ADDR_SHIFT	(12U)
-#define TABLE_ADDR_WIDTH	(48U - TABLE_ADDR_SHIFT)
-#define TABLE_ADDR_MASK		MASK(TABLE_ADDR)
+#define AP2_SHIFT		UL(0x7)
+#define AP2_RO			ULL(0x1)
+#define AP2_RW			ULL(0x0)
 
-#define AP2_SHIFT			UL(0x7)
-#define AP2_RO				ULL(0x1)
-#define AP2_RW				ULL(0x0)
-
-#define AP1_SHIFT			UL(0x6)
+#define AP1_SHIFT		UL(0x6)
 
 /*
  * The following definitions must all be passed to the LOWER_ATTRS() macro to
@@ -87,13 +61,26 @@
 #define AP_RW				(AP2_RW << UL(5))
 #define AP_ACCESS_UNPRIVILEGED		(AP1_ACCESS_UNPRIVILEGED    << UL(4))
 #define AP_NO_ACCESS_UNPRIVILEGED	(AP1_NO_ACCESS_UNPRIVILEGED << UL(4))
-#define ATTR_NON_CACHEABLE_INDEX	UL(0x2)
 #define ATTR_DEVICE_INDEX		UL(0x1)
 #define ATTR_IWBWA_OWBWA_NTR_INDEX	UL(0x0)
 #define NG_HINT				(ULL(1) << 9)
 
-/* Normal Memory, Outer Write-Through non-transient, Inner Non-cacheable */
-#define ATTR_NON_CACHEABLE		MAKE_MAIR_NORMAL_MEMORY(MAIR_NORM_NC, MAIR_NORM_NC)
+/*
+ * Shareability attributes within the lower attributes
+ * field of the TTE.
+ *
+ * Only ISH Shareability is supported.
+ */
+#define LOWER_ATTR_SH_SHIFT	(6U)
+#define LOWER_ATTR_SH_WIDTH	(2U)
+
+/*
+ * Shareability attributes in TCR_EL2 when FEAT_LPA2 is enabled.
+ *
+ * Only ISH is supported.
+ */
+#define ISH			(UL(0x3))
+
 /* Device-nGnRE */
 #define ATTR_DEVICE			MAIR_DEV_NGNRE
 /* Normal Memory, Outer Write-Back non-transient, Inner Write-Back non-transient */
@@ -102,27 +89,57 @@
 #define ATTR_INDEX_MASK			U(0x3)
 #define ATTR_INDEX_GET(attr)		(((attr) >> UL(2)) & ATTR_INDEX_MASK)
 
-/*
- * Shift values for the attributes fields in a block or page descriptor.
- * See section D4.3.3 in the ARMv8-A ARM (issue B.a).
- */
+/* Output address field on a TTE given 4KB granularity. */
+#define OA_ADDR_SHIFT		(12U)
 
-/* Memory attributes index field, AttrIndx[2:0]. */
-#define ATTR_INDEX_SHIFT		2
-/* Non-secure bit, NS. */
-#define NS_SHIFT			5
-/* Shareability field, SH[1:0] */
-#define SHAREABILITY_SHIFT		8
-/* The Access Flag, AF. */
-#define ACCESS_FLAG_SHIFT		10
-/* The not global bit, nG. */
-#define NOT_GLOBAL_SHIFT		11
-/* Non-secure extension bit. Only valid in the EL3 translation regime */
-/* Contiguous hint bit. */
-#define CONT_HINT_SHIFT			52
-/* Execute-never bits, XN. */
-#define PXN_SHIFT			53
-#define XN_SHIFT			54
-#define UXN_SHIFT			XN_SHIFT
+#define TTE_OA_ADDR_MSB		(47U)
+
+/*
+ * Table descriptor format for 52 bit OA  is [49:12] for the bits [49:12]
+ * of the table address. For bits [51:50] it is [9:8] of descriptor.
+ * See D8.3.1 Table descriptor format in Issue I.a of Arm ARM.
+ */
+#define TTE_OA_BIT_49_LPA2	(49U)
+
+/*
+ * When FEAT_LPA2 is enabled bits [51:50] of the OA are
+ * bits [9:8] on the TTE.
+ */
+#define TTE_OA_BITS_50_51_SHIFT		ULL(8)
+#define TTE_OA_BITS_50_51_WIDTH		ULL(2)
+
+/* Bitfields for the MSBs on an OA */
+#define OA_BITS_50_51_SHIFT	ULL(51)
+#define OA_BITS_50_51_WIDTH	TTE_OA_BITS_50_51_WIDTH
+#define OA_BITS_50_51_MASK	MASK(OA_BITS_50_51)
+
+/* Different address masks */
+#define ADDR_MASK_52_TO_63	BIT_MASK_ULL(U(63), U(52))
+#define ADDR_MASK_48_TO_51	BIT_MASK_ULL(U(51), U(48))
+#define ADDR_MASK_44_TO_47	BIT_MASK_ULL(U(47), U(44))
+#define ADDR_MASK_42_TO_43	BIT_MASK_ULL(U(43), U(42))
+#define ADDR_MASK_40_TO_41	BIT_MASK_ULL(U(41), U(40))
+#define ADDR_MASK_36_TO_39	BIT_MASK_ULL(U(39), U(36))
+#define ADDR_MASK_32_TO_35	BIT_MASK_ULL(U(35), U(32))
+
+#define SET_TCR_SH(_region, _sh)	\
+		(((_region) == VA_LOW_REGION) ? INPLACE(TCR_EL2_SH0, (_sh)) : \
+						INPLACE(TCR_EL2_SH1, (_sh)))
+
+/* Helper function to extract the OA on a TTE */
+static inline uint64_t *get_oa_from_tte(uint64_t tte)
+{
+	uint64_t oa;
+
+	if (is_feat_lpa2_4k_present() == true) {
+		oa = BIT_MASK_ULL(TTE_OA_BIT_49_LPA2, OA_ADDR_SHIFT);
+		oa &= tte;
+		oa |= INPLACE(OA_BITS_50_51, EXTRACT(TTE_OA_BITS_50_51, tte));
+	} else {
+		oa = BIT_MASK_ULL(TTE_OA_ADDR_MSB, OA_ADDR_SHIFT);
+		oa &= tte;
+	}
+	return (uint64_t *)(void *)oa;
+}
 
 #endif /* XLAT_DEFS_PRIVATE_H */
