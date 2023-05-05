5e223d74ee8d2daff897d1d14949c1aa58941571
Javier Almansa Sobrino <javier.almansasobrino@arm.com>
2023-05-05 20:36:26 +0100

    feat(s2tte): add support for FEAT_LPA2 to the s2tte library
    Signed-off-by: Javier Almansa Sobrino <javier.almansasobrino@arm.com>
    Change-Id: I1db0e7b9ce9ab5c67fd8a5027fbca27630ed85aa

--- lib/realm/include/table.h
+++ lib/realm/include/table.h
@@ -13,15 +13,13 @@
 #define MAX_IPA_BITS		48
 #define MAX_IPA_SIZE		(1UL << MAX_IPA_BITS)
 
+#define MAX_IPA_BITS_LPA2	52
+#define MAX_IPA_SIZE_LPA2	(1UL << MAX_IPA_BITS_LPA2)
+
 #define MIN_STARTING_LEVEL	0
+#define MIN_STARTING_LEVEL_LPA2	-1
 #define RTT_PAGE_LEVEL		3
 #define RTT_MIN_BLOCK_LEVEL	2
-
-/* TODO: Fix this when introducing LPA2 support */
-COMPILER_ASSERT(MIN_STARTING_LEVEL >= 0);
-
-/* TODO: Allow the NS caller to select the stage 2 starting level */
-#define RTT_STARTING_LEVEL	0
 
 /*
  * S2TTE_STRIDE: The number of bits resolved in a single level of translation
@@ -118,9 +116,7 @@ static inline uint64_t __tte_read(uint64_t *ttep)
  */
 static inline unsigned int max_ipa_size(void)
 {
-	unsigned int ipa_size = arch_feat_get_pa_width();
-
-	return ipa_size > MAX_IPA_BITS ? MAX_IPA_BITS : ipa_size;
+	return arch_feat_get_pa_width();
 }
 
 #endif /* TABLE_H */
