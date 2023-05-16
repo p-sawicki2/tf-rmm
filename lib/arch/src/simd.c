819a5c88657fe8243a84054170bed41cf8867475
Arunachalam Ganapathy <arunachalam.ganapathy@arm.com>
2023-05-16 16:53:52 +0100

    feat(lib/arch): add support for SME
    This feature adds support for Scalable Matrix Extension (SME) in RMM for
    handling the Non Secure SME state. If the CPU supports SME and if Realm
    accesses FPU/SVE functionality then NS SME state is saved to allow Realm
    to use FPU/SVE register state.
    
    Within SME, only the Streaming SVE register state is managed by RMM, as
    it shares the register state with FPU/SVE. As Realms do not support SME,
    the ZA register state is never managed.
    
    This change also caters to the use case of RMM using FPU at REL2 when
    built with RMM_FPU_USE_AT_REL2=ON.
    
    Signed-off-by: Arunachalam Ganapathy <arunachalam.ganapathy@arm.com>
    Change-Id: I1250e6b37d1f1256363ea39d6eb770f510c700f7

--- lib/arch/src/simd.c
+++ lib/arch/src/simd.c
@@ -11,22 +11,28 @@
 #include <simd_private.h>
 
 /*
- * Global to store the max vq length supported by the CPU. We expect all CPUs
- * in the system to support the same max vq length.
+ * Global variable to store the SVE max vq length supported by the CPU. We
+ * expect all CPUs in the system to support the same max vq length.
  */
 static int32_t g_sve_max_vq = -1;
 
 /*
- * The default CPU simd type is set as FPU. During init time, if CPU supports
- * SVE (which encompasses FPU), this will be updated to SIMD_SVE.
+ * The default CPU SIMD type is set as FPU. During init time, if CPU supports
+ * SVE (which encompasses FPU), this will be updated to SIMD_SVE. And if CPU
+ * supports SME (which encompasses part of SVE), then this will be updated to
+ * SIMD_SME.
  */
 static simd_t g_cpu_simd_type = SIMD_FPU;
 
-/* This structure is used for storing FPU or SVE context for NS world. */
+/*
+ * This structure is used for storing NS world FPU or SVE or Streaming SVE
+ * context.
+ */
 struct ns_simd_state {
 	struct simd_state simd;
 	uint64_t ns_zcr_el2;
-	bool saved;
+	bool ssve_mode;		/* Is NS in Streaming SVE mode */
+	bool saved;		/* Is NS SIMD state (FPU/SVE/SME) saved */
 } __attribute__((aligned(CACHE_WRITEBACK_GRANULE)));
 
 static struct ns_simd_state g_ns_simd[MAX_CPUS];
@@ -93,6 +99,21 @@ void simd_save_state(simd_t type, struct simd_state *simd)
 		 */
 		sve_save_state((uint64_t)&simd->t.sve, true);
 		break;
+	case SIMD_SME:
+		assert(is_feat_sme_present() == true);
+		assert(is_smen_enabled() && is_zen_enabled() &&
+		       is_fpen_enabled());
+		assert(sme_smstat());
+
+		/*
+		 * Streaming SVE state is saved with the existing SVL in SMCR_EL2
+		 * that was programmed by NS world. RMM doesn't modify
+		 * SMCR_EL2.LEN bits.
+		 */
+
+		/* Save Streaming SVE state */
+		sve_save_state((uint64_t)&simd->t.sve, sme_feat_fa64_enabled());
+		break;
 	default:
 		assert(false);
 	}
@@ -131,6 +152,22 @@ void simd_restore_state(simd_t type, struct simd_state *simd)
 		 */
 		sve_restore_state((uint64_t)&simd->t.sve, true);
 		break;
+	case SIMD_SME:
+		assert(is_feat_sme_present() == true);
+		assert(is_smen_enabled() && is_zen_enabled() &&
+		       is_fpen_enabled());
+		assert(simd->simd_type == SIMD_SME && sme_smstat());
+
+		/*
+		 * Streaming SVE state is restored with the existing SVL in
+		 * SMCR_EL2 that was programmed by NS world. RMM doesn't modify
+		 * SMCR_EL2.LEN bits.
+		 */
+
+		/* restore Streaming SVE state */
+		sve_restore_state((uint64_t)&simd->t.sve,
+				  sme_feat_fa64_enabled());
+		break;
 	default:
 		assert(false);
 	}
@@ -147,6 +184,7 @@ void simd_save_ns_state(void)
 {
 	unsigned int cpu_id = my_cpuid();
 	simd_t stype;
+	bool ssve_mode = false;
 
 	assert(g_ns_simd[cpu_id].saved == false);
 	stype = g_cpu_simd_type;
@@ -155,11 +193,50 @@ void simd_save_ns_state(void)
 	/*
 	 * Save the NS zcr_el2 value since EL3 doesn't bank this. Note that the
 	 * save of NS SVE context occurs with MAX_SVE_VL to prevent leakage.
+	 *
+	 * If CPU supports SME (which encompasses SVE), the NS_ZCR_EL2 is saved
+	 * as Realms might use FPU or SVE.
 	 */
-	if (stype == SIMD_SVE) {
+	if (stype == SIMD_SVE || stype == SIMD_SME) {
 		g_ns_simd[cpu_id].ns_zcr_el2 = read_zcr_el2();
 	}
+
+	/*
+	 * As Realms do not support SME, the ZA state (for FEAT_SME) or ZT0
+	 * (for FEAT_SME2) are not saved. The PSTATE.ZA is not accessed by RMM.
+	 *
+	 * As Realms support FPU/SVE, when the CPU is in Streaming SVE mode
+	 * (PSTATE.SM is 1), the Streaming SVE state is saved.
+	 *
+	 * When SME is supported and the CPU not in Streaming SVE mode, then SVE
+	 * state is saved.
+	 */
+	if (stype == SIMD_SME) {
+		ssve_mode = sme_smstat();
+
+		if (!ssve_mode) {
+			/* SIMD type to save is SVE, so enable traps for SME */
+			stype = SIMD_SVE;
+			simd_enable(SIMD_SVE);
+		}
+
+		/* Set Streaming mode status flag in per-cpu data */
+		g_ns_simd[cpu_id].ssve_mode = ssve_mode;
+	}
+
 	simd_save_state(stype, &g_ns_simd[cpu_id].simd);
+
+	/*
+	 * After saving the state, exit Streaming SVE mode, if entered with
+	 * Streaming SVE mode enabled. This makes FPU/SVE accessible by Realms.
+	 *
+	 * Exiting Streaming SVE mode doesn't have any impact on SME ZA storage
+	 * or, if implemented, ZT0 storage.
+	 */
+	if ((stype == SIMD_SME) && ssve_mode) {
+		sme_smstop();
+	}
+
 	simd_disable();
 	g_ns_simd[cpu_id].saved = true;
 }
@@ -178,16 +255,38 @@ void simd_restore_ns_state(void)
 	stype = g_cpu_simd_type;
 
 	simd_enable(stype);
+
+	if (stype == SIMD_SME) {
+		if (!g_ns_simd[cpu_id].ssve_mode) {
+			stype = SIMD_SVE;
+			simd_enable(SIMD_SVE);
+		} else {
+			/*
+			 * Upon entering Streaming SVE mode each implemented bit
+			 * of SVE registers Z0-Z31, P0-P15, and FFR in the new
+			 * mode is set to zero. Even if Normal SVE implements a
+			 * greater vector length than Streaming SVE, always the
+			 * whole state is cleared.
+			 *
+			 * Entering Streaming SVE mode doesn't have any impact
+			 * on SME ZA storage or, if implemented, ZT0 storage.
+			 */
+			sme_smstart();
+		}
+	}
+
 	simd_restore_state(stype, &g_ns_simd[cpu_id].simd);
+
 	/*
 	 * Note that the restore SVE state for NS state happens with
 	 * MAX_SVE_VL to prevent leakage. RMM now needs to restore the NS zcr_el2
 	 * value since EL3 is not saving this.
 	 */
-	if (stype == SIMD_SVE) {
+	if (stype == SIMD_SVE || stype == SIMD_SME) {
 		write_zcr_el2(g_ns_simd[cpu_id].ns_zcr_el2);
 		isb();
 	}
+
 	simd_disable();
 	g_ns_simd[cpu_id].saved = false;
 }
@@ -223,16 +322,19 @@ void simd_state_init(simd_t type, struct simd_state *simd, uint8_t sve_vq)
 }
 
 /* Find the SVE max vector length restricted by EL3 */
-static void sve_init(void)
+static void sve_init_once(void)
 {
-	/* Check if g_sve_max_vq is initialized */
+	/*
+	 * Called only once during cold boot. Check if 'g_sve_max_vq' is already
+	 * initialized.
+	 */
 	if (g_sve_max_vq != -1) {
 		return;
 	}
 
 	/*
 	 * Write architecture max limit for vector length to ZCR_EL2.LEN and read
-	 * back the sve vector length reported by rdvl. This will give the vector
+	 * back the SVE vector length reported by RDVL. This will give the vector
 	 * length limit set by EL3 for its lower ELs. RMM will use this value as
 	 * max limit for EL2 and lower ELs.
 	 */
@@ -249,18 +351,64 @@ static void sve_init(void)
 	simd_disable();
 }
 
+/* Find the architecturally permitted (Streaming Vector Length) SVL */
+static void sme_init_once(void)
+{
+	static int32_t sme_svq_arch_max = -1;
+	uint64_t smcr_val;
+
+	/*
+	 * Called only once during cold boot. Check if 'sme_svq_arch_max' is
+	 * already initialized.
+	 */
+	if (sme_svq_arch_max != -1) {
+		return;
+	}
+
+	simd_enable(SIMD_SME);
+
+	/*
+	 * Arm SME supplement recommends to request an out of range vector
+	 * length of 8192 bytes by writing 0x1ff to SMCR_ELx[8:0]. Reading back
+	 * the register will give the max architecturally permitted SVQ.
+	 *
+	 * The old smcr_el2 is not restored as this is called only once during
+	 * cold boot.
+	 */
+	smcr_val = read_smcr_el2();
+	smcr_val |= MASK(SMCR_EL2_RAZ_LEN);
+	write_smcr_el2(smcr_val);
+	isb();
+	sme_svq_arch_max = EXTRACT(SMCR_EL2_RAZ_LEN, read_smcr_el2());
+
+	/*
+	 * Streaming SVE shares SVE register set Z/P/FFR. RMM will save and
+	 * restore Streaming SVE state in 'sve_state', and 'sve_state' can hold
+	 * vector registers for up to 2048 bits (vq: 15). If Streaming SVE mode
+	 * reports a larger value than SVE_VQ_ARCH_MAX, then assert. Hopefully
+	 * we won't hit this condition in the near future.
+	 */
+	assert(sme_svq_arch_max <= SVE_VQ_ARCH_MAX);
+
+	g_cpu_simd_type = SIMD_SME;
+	simd_disable();
+}
+
 /*
  * This function initializes the SIMD layer depending on SIMD capability of the
- * CPU (FPU/SVE). If CPU supports SVE, the max VQ will be discovered and NS SIMD
- * SVE state will be initialized with the max vq
+ * CPU (FPU/SVE/SME).
  */
 void simd_init(void)
 {
 	unsigned int cpu_id = my_cpuid();
 
 	if (is_feat_sve_present()) {
-		sve_init();
+		sve_init_once();
 		/* Set the max vq in NS simd state */
 		g_ns_simd[cpu_id].simd.t.sve.vq = g_sve_max_vq;
+	}
+
+	if (is_feat_sme_present()) {
+		sme_init_once();
 	}
 }
