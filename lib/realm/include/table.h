fa23533eb2543755f4255ba141cd7d08506ae833
Mate Toth-Pal <mate.toth-pal@arm.com>
2023-06-20 16:03:53 +0200

    refactor(cbmc): make some functions/variables non-static
    Change-Id: Icc4271c53ac4889d88299d42309d2d965e25061d
    Signed-off-by: Mate Toth-Pal <mate.toth-pal@arm.com>

--- lib/realm/include/table.h
+++ lib/realm/include/table.h
@@ -123,4 +123,9 @@ static inline unsigned int max_ipa_size(void)
 	return ipa_size > MAX_IPA_BITS ? MAX_IPA_BITS : ipa_size;
 }
 
+#ifdef CBMC
+unsigned long s2_addr_to_idx(unsigned long addr, long level);
+unsigned long s2_sl_addr_to_idx(unsigned long addr, int start_level, unsigned long ipa_bits);
+#endif
+
 #endif /* TABLE_H */
