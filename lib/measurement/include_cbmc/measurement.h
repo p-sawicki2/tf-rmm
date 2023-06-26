09435244789e7ddcfb9777f8561ae4b518d04d6c
Mate Toth-Pal <mate.toth-pal@arm.com>
2023-06-26 08:22:54 +0200

    feat(cbmc): add cbmc includes for lib measurement
    Change-Id: If35f35069f8c473450b700bd66d976a1e15254ae
    Signed-off-by: Mate Toth-Pal <mate.toth-pal@arm.com>

---
+++ lib/measurement/include_cbmc/measurement.h
@@ -0,0 +1,59 @@
+/*
+ * SPDX-License-Identifier: BSD-3-Clause
+ * SPDX-FileCopyrightText: Copyright TF-RMM Contributors.
+ */
+
+#ifndef MEASUREMENT_H
+#define MEASUREMENT_H
+
+#include <smc-rmi.h>
+#include <stddef.h>
+
+enum hash_algo {
+	HASH_ALGO_SHA256 = RMI_HASH_ALGO_SHA256,
+	HASH_ALGO_SHA512 = RMI_HASH_ALGO_SHA512,
+};
+
+#define RIM_MEASUREMENT_SLOT		(0U)
+#define MEASUREMENT_SLOT_NR		(1U)
+#define MAX_MEASUREMENT_SIZE		8
+
+static void measurement_hash_compute(enum hash_algo hash_algo,
+			      void *data,
+			      size_t size, unsigned char *out)
+{}
+
+static void measurement_extend(enum hash_algo hash_algo,
+			void *current_measurement,
+			void *extend_measurement,
+			size_t extend_measurement_size,
+			unsigned char *out)
+{}
+
+static inline size_t measurement_get_size(const enum hash_algo algorithm)
+{
+	return 8UL;
+}
+
+static
+void measurement_data_granule_measure(unsigned char rim_measurement[],
+				      enum hash_algo algorithm,
+				      void *data,
+				      unsigned long ipa,
+				      unsigned long flags)
+{}
+
+static
+void measurement_ripas_granule_measure(unsigned char rim_measurement[],
+				       enum hash_algo algorithm,
+				       unsigned long ipa,
+				       unsigned long level)
+{}
+
+static
+void measurement_rec_params_measure(unsigned char rim_measurement[],
+				    enum hash_algo algorithm,
+				    struct rmi_rec_params *rec_params)
+{}
+
+#endif /* MEASUREMENT_H */
