272ef0c61429fb8597c404172aa21c6b8ea42e6e
Mate Toth-Pal <mate.toth-pal@arm.com>
2023-06-27 13:08:31 +0200

    feat(cbmc): add cbmc includes for lib attestation
    Change-Id: Ic25736a2047a515d6e7bd6403cf964b0dacb11f5
    Signed-off-by: Mate Toth-Pal <mate.toth-pal@arm.com>

---
+++ lib/attestation/include_cbmc/attestation.h
@@ -0,0 +1,17 @@
+/*
+ * SPDX-License-Identifier: BSD-3-Clause
+ * SPDX-FileCopyrightText: Copyright TF-RMM Contributors.
+ */
+
+#ifndef ATTESTATION_H
+#define ATTESTATION_H
+
+#include <stddef.h>
+
+static int
+attestation_heap_reinit_pe(unsigned char *buf, size_t buf_size)
+{
+	return 0;
+}
+
+#endif /* ATTESTATION_H */
