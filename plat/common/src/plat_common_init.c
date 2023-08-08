96a8623f96eb2c1f73668dd9fb5bfa915f63cd62
Mate Toth-Pal <mate.toth-pal@arm.com>
2023-08-08 13:49:19 +0200

    feat(runtime): for each CPU only map their own stack respectively
    This commit changes the memory mapping of the stack area. Instead of
    flat-mapping the whole stack area for each CPU, only map the stack
    area for that particular CPU in the High region.
    
    For this to work, the following changes are made:
     - move the mapping of the high region out from the realm lib to the
       platform
     - move the build time config option RMM_NUM_PAGES_PER_STACK up in the
       folder hierarchy, so that it is available in the plat folder as well.
     - add a new mm region to the high VA space for the stack of that CPU.
     - reorder segments in the linker script so that `.percpu` segment is
       the last. Change flat-mapping configurations so that the `.percpu`
       segment is not added.
    
    Change-Id: I997c1b5ff2b972807d75692b5efc9075c5a29a30
    Signed-off-by: Mate Toth-Pal <mate.toth-pal@arm.com>

--- plat/common/src/plat_common_init.c
+++ plat/common/src/plat_common_init.c
@@ -24,14 +24,14 @@ IMPORT_SYM(uintptr_t, rmm_text_end, RMM_CODE_END);
 IMPORT_SYM(uintptr_t, rmm_ro_start, RMM_RO_START);
 IMPORT_SYM(uintptr_t, rmm_ro_end, RMM_RO_END);
 IMPORT_SYM(uintptr_t, rmm_rw_start, RMM_RW_START);
-IMPORT_SYM(uintptr_t, rmm_rw_end, RMM_RW_END);
+IMPORT_SYM(uintptr_t, rmm_rw_flat_end, RMM_RW_FLAT_END);
 
 /*
  * Memory map REGIONS used for the RMM runtime (static mappings)
  */
 #define RMM_CODE_SIZE		(RMM_CODE_END - RMM_CODE_START)
 #define RMM_RO_SIZE		(RMM_RO_END - RMM_RO_START)
-#define RMM_RW_SIZE		(RMM_RW_END - RMM_RW_START)
+#define RMM_RW_FLAT_SIZE	(RMM_RW_FLAT_END - RMM_RW_START)
 
 #define RMM_CODE		MAP_REGION_FLAT(			\
 					RMM_CODE_START,			\
@@ -45,7 +45,7 @@ IMPORT_SYM(uintptr_t, rmm_rw_end, RMM_RW_END);
 
 #define RMM_RW			MAP_REGION_FLAT(			\
 					RMM_RW_START,			\
-					RMM_RW_SIZE,			\
+					RMM_RW_FLAT_SIZE,		\
 					MT_RW_DATA | MT_REALM)
 
 /*
@@ -53,7 +53,7 @@ IMPORT_SYM(uintptr_t, rmm_rw_end, RMM_RW_END);
  * of the shared buffer VA. This will help to detect any memory access
  * underflow by RMM.
  */
-#define RMM_SHARED_BUFFER_START	(RMM_RW_END + SZ_4K)
+#define RMM_SHARED_BUFFER_START	(RMM_RW_FLAT_END + SZ_4K)
 
 /*
  * Some of the fields for the RMM_SHARED region will be populated
@@ -71,6 +71,22 @@ IMPORT_SYM(uintptr_t, rmm_rw_end, RMM_RW_END);
 /* Total number of memory mapping regions */
 #define TOTAL_MMAP_REGIONS	(COMMON_REGIONS + PLAT_CMN_EXTRA_MMAP_REGIONS)
 
+#define STACK_ATTR		(MT_RW_DATA | MT_NG)
+
+/* Leave some pages of gap above the stack top */
+#define CPU_STACK_GAP		(16ULL * 0x1000ULL)
+
+#define CPU_STACK_SIZE		(RMM_NUM_PAGES_PER_STACK * GRANULE_SIZE)
+
+#define RMM_STACK_MMAP		MAP_REGION_FULL_SPEC(			\
+				0, /* PA is different for each CPU */	\
+				0, /* VA is calculated based on slot buf VA */ \
+				CPU_STACK_SIZE,				\
+				STACK_ATTR,				\
+				PAGE_SIZE)
+
+#define MMAP_REGION_COUNT	2U
+
 /* Memory mapping regions for the system runtime */
 static struct xlat_mmap_region static_regions[TOTAL_MMAP_REGIONS];
 
@@ -87,6 +103,33 @@ static uint64_t static_s1tt[XLAT_TABLE_ENTRIES * PLAT_CMN_CTX_MAX_XLAT_TABLES]
 static struct xlat_ctx_tbls runtime_tbls;
 static struct xlat_ctx_cfg runtime_xlat_ctx_cfg;
 static struct xlat_ctx runtime_xlat_ctx;
+
+/*
+ * The base tables for all the contexts are manually allocated as a continuous
+ * block of memory (one L3 table per PE).
+ */
+static uint64_t high_va_s1tt[XLAT_TABLE_ENTRIES * MAX_CPUS]
+				    __aligned(XLAT_TABLES_ALIGNMENT);
+
+/* Allocate per-cpu xlat_ctx_tbls */
+static struct xlat_ctx_tbls high_va_tbls[MAX_CPUS];
+
+/* Allocate xlat_ctx_cfg for high VA which will be specific to PEs */
+static struct xlat_ctx_cfg high_va_xlat_ctx_cfgs[MAX_CPUS];
+
+/* context definition */
+static struct xlat_ctx high_va_xlat_ctx[MAX_CPUS];
+
+/*
+ * Global symbol set by the C code
+ * The stack bottom VA is calculated using CLZ operation which is not supported
+ * by the preprocessor. So the C code sets the address in this variable, and
+ * the assembly init function loads it from here when setting SP to the stack
+ * VA.
+ */
+uintptr_t cpu_stack_bottom_va;
+
+extern unsigned long stack_end[];
 
 /*
  * Platform common cold boot setup for RMM.
@@ -179,8 +222,119 @@ int plat_cmn_setup(unsigned long x0, unsigned long x1,
 	/* Read supported GIC virtualization features and init GIC variables */
 	gic_get_virt_features();
 
-	/* Perform coold boot initialization of the slot buffer mechanism */
-	return slot_buf_coldboot_init();
+	return 0;
+}
+
+static void copy_to_region(
+	struct xlat_mmap_region mm_regions[MMAP_REGION_COUNT],
+	unsigned int index,
+	struct xlat_mmap_region *mm_region)
+{
+	assert(index < MMAP_REGION_COUNT);
+	memcpy(&(mm_regions[index]),
+	       mm_region,
+	       sizeof(struct xlat_mmap_region));
+}
+
+/* inline */ struct xlat_ctx *get_high_va_xlat_ctx(void)
+{
+	return &high_va_xlat_ctx[my_cpuid()];
+}
+
+static int high_va_warmboot_init(void)
+{
+	static struct xlat_mmap_region
+		mm_regions_array[MAX_CPUS][MMAP_REGION_COUNT];
+	struct xlat_mmap_region rmm_slot_buf_mmap;
+	struct xlat_mmap_region rmm_stack_mmap = RMM_STACK_MMAP;
+	unsigned int cpuid = my_cpuid();
+	uintptr_t slot_virt;
+	uintptr_t stack_virt;
+	size_t used_va_size;
+	size_t rounded_va_size;
+	size_t index = 0;
+	int ret;
+
+	slot_buf_get_mmap(&rmm_slot_buf_mmap);
+
+	slot_virt = rmm_slot_buf_mmap.base_va;
+	stack_virt = slot_virt - CPU_STACK_GAP - CPU_STACK_SIZE;
+
+	cpu_stack_bottom_va =
+		stack_virt + (RMM_NUM_PAGES_PER_STACK * GRANULE_SIZE);
+
+	/* Set stack VA for this CPU */
+	rmm_stack_mmap.base_pa =
+		(unsigned long)stack_end - (cpuid + 1U) * CPU_STACK_SIZE;
+	rmm_stack_mmap.base_va = stack_virt;
+
+	copy_to_region(mm_regions_array[cpuid], index++, &rmm_stack_mmap);
+	copy_to_region(mm_regions_array[cpuid], index++, &rmm_slot_buf_mmap);
+
+	/*
+	 * The VA space size for the high region needs to be a power of two, so
+	 * calculate the size of the used VA space and round that up t the
+	 * closest power or two.
+	 */
+	used_va_size = (ULL(0xffffffffffffffff) -
+		mm_regions_array[cpuid][0U].base_va + 1);
+	rounded_va_size = 1ULL << (64ULL - __builtin_clzll(used_va_size - 1));
+
+	/*
+	 * For all translation stages if FEAT_TTST is implemented, while
+	 * the PE is executing in AArch64 state and is using 4KB
+	 * translation granules, the min address space size is 64KB
+	 */
+	assert((rounded_va_size) >= (1 << 16U));
+
+	/*
+	 * Initialize the common configuration used for all
+	 * translation contexts
+	 */
+	ret = xlat_ctx_cfg_init(&high_va_xlat_ctx_cfgs[cpuid], VA_HIGH_REGION,
+				&mm_regions_array[cpuid][0U],
+				index,
+				rounded_va_size);
+	if (ret != 0) {
+		return ret;
+	}
+	return 0;
+}
+
+/*
+ * Setup xlat table for the high VA region for each PE.
+ * Must be called for every PE in the system
+ */
+void high_va_setup_xlat(void)
+{
+	unsigned int cpuid = my_cpuid();
+	struct xlat_ctx *high_va_ctx = get_high_va_xlat_ctx();
+
+	/*
+	 * Initialize the translation tables for the current context.
+	 * This is done on the first boot of each PE.
+	 */
+	int ret = xlat_ctx_init(high_va_ctx,
+				&high_va_xlat_ctx_cfgs[cpuid],
+				&high_va_tbls[cpuid],
+				&high_va_s1tt[XLAT_TABLE_ENTRIES * cpuid], 1U);
+
+	if (!((ret == 0) || (ret == -EALREADY))) {
+		/*
+		 * If the context was already created, carry on with the
+		 * initialization. If it cannot be created, panic.
+		 */
+		ERROR("%s (%u): Failed to initialize the xlat context for the slot buffers (-%i)\n",
+					__func__, __LINE__, ret);
+		panic();
+	}
+
+	/* Configure MMU registers */
+	if (xlat_arch_setup_mmu_cfg(get_high_va_xlat_ctx())) {
+		ERROR("%s (%u): MMU registers failed to initialize\n",
+					__func__, __LINE__);
+		panic();
+	}
 }
 
 /*
@@ -202,8 +356,16 @@ int plat_cmn_warmboot_setup(void)
 		return ret;
 	}
 
-	/* Setup the MMU cfg for the slot buffer context (high region) */
-	slot_buf_setup_xlat();
+	/* Perform warm boot initialization of the high VA region */
+	ret = high_va_warmboot_init();
+	if (ret != 0) {
+		ERROR("%s (%u): Failed to setup high va xlat tables for CPU[%u]\n",
+			__func__, __LINE__, my_cpuid());
+		return ret;
+	}
+
+	/* Setup the MMU cfg for the high region */
+	high_va_setup_xlat();
 
 	VERBOSE("xlat tables configured for CPU[%u]\n", my_cpuid());
 	return 0;
