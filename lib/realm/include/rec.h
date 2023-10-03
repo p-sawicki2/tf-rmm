ae194bda28582ee92a29a1420812097d7f5046cd
AlexeiFedorov <Alexei.Fedorov@arm.com>
2023-10-03 11:11:47 +0100

    feat(rmm-eac4): modify RSI_ATTESTATION_TOKEN commands
    Modifies RSI_ATTESTATION_TOKEN_INIT and
    RSI_ATTESTATION_TOKEN_CONTINUE commands as per
    RMM Specification 1.0-eac4.
    Number of 4K pages allocated for attestation
    buffer is defined by RMM_ATTEST_BUFFER_PAGES
    in CommonConfigs.cmake and set to 1 by default.
    
    Signed-off-by: AlexeiFedorov <Alexei.Fedorov@arm.com>
    Change-Id: Icda7f26e291a19b143ae14a291c03cc7bda0e076

--- lib/realm/include/rec.h
+++ lib/realm/include/rec.h
@@ -131,6 +131,9 @@ struct rec_aux_data {
 
 	/* Pointer to attestation-related data */
 	struct rec_attest_data *attest_data;
+
+	/* Pointer to the attestation buffer */
+	uint8_t *attest_buffer;
 };
 
 struct rec {
