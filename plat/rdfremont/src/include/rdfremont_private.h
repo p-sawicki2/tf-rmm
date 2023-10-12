2b03f6e42e9128aae06e3fc8c09ec764b5e2bc37
Harry Moulton <harry.moulton@arm.com>
2023-10-12 12:17:33 +0100

    plat(rdfremont): introduce initial support for rdfremont platform.
    Introduce initial support for the RD-Fremont platform in RMM.
    
    Signed-off-by: Harry Moulton <harry.moulton@arm.com>
    Signed-off-by: Sathyam Panda <sathyam.panda@arm.com>
    Signed-off-by: Vivek Gautam <vivek.gautam@arm.com>
    Change-Id: I012b0aee11d2baf7a6195dc3e595289cf09a068e

---
+++ plat/rdfremont/src/include/rdfremont_private.h
@@ -0,0 +1,16 @@
+/*
+ * SPDX-License-Identifier: BSD-3-Clause
+ * SPDX-FileCopyrightText: Copyright TF-RMM Contributors.
+ */
+#ifndef RDFREMONT_PRIVATE_H
+#define RDFREMONT_PRIVATE_H
+
+/* Default number of CPUs per cluster */
+#define	RDFREMONT_MAX_CPUS_PER_CLUSTER		4
+
+/* Default number of threads per CPU */
+#define	RDFREMONT_MAX_PE_PER_CPU		1
+#define RDFREMONT_UART_BAUDRATE			115200
+#define RDFREMONT_UART_CLK_IN_HZ		14745600
+
+#endif /* RDFREMONT_PRIVATE_H */
