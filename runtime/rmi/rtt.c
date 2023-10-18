1b7e7f83252012cca9d27d7f93a5aa2c104b64e6
Javier Almansa Sobrino <javier.almansasobrino@arm.com>
2023-10-18 18:25:56 +0100

    chore(s2tt): optimize s2tt component
    This patch introduces a minor set of optimizations into the
    s2tt and rtt components in order to improve performance.
    Mainly, the optimizations consist on define some very simple
    APIs as 'inline' to remove the calling/return overhead when
    possible, as soon as other minor modifications.
    
    Signed-off-by: Javier Almansa Sobrino <javier.almansasobrino@arm.com>
    Change-Id: I8c96e76badeca560db316853ab1730df332a3474

--- runtime/rmi/rtt.c
+++ runtime/rmi/rtt.c
@@ -5,6 +5,7 @@
  */
 
 #include <buffer.h>
+#include <errno.h>
 #include <granule.h>
 #include <measurement.h>
 #include <realm.h>
@@ -137,7 +138,6 @@ unsigned long smc_rtt_create(unsigned long rd_addr,
 	parent_s2tte = s2tte_read(&parent_s2tt[wi.index]);
 	s2tt = granule_map(g_tbl, SLOT_DELEGATED);
 	assert(s2tt != NULL);
-
 
 	if (s2tte_is_unassigned_empty(parent_s2tte)) {
 		s2tt_init_unassigned_empty(s2tt);
@@ -1126,7 +1126,7 @@ static int update_ripas(unsigned long *s2ttep, long level,
 	int ret = 0;
 
 	if (!s2tte_has_ripas(s2tte, level)) {
-		return -1;
+		return -EINVAL;
 	}
 
 	if (ripas_val == RIPAS_RAM) {
@@ -1136,7 +1136,7 @@ static int update_ripas(unsigned long *s2ttep, long level,
 			if (change_destroyed == CHANGE_DESTROYED) {
 				s2tte = s2tte_create_unassigned_ram();
 			} else {
-				return -1;
+				return -EPERM;
 			}
 		} else if (s2tte_is_assigned_empty(s2tte, level)) {
 			pa = s2tte_pa(s2tte, level);
@@ -1146,7 +1146,7 @@ static int update_ripas(unsigned long *s2ttep, long level,
 				pa = s2tte_pa(s2tte, level);
 				s2tte = s2tte_create_assigned_ram(pa, level);
 			} else {
-				return -1;
+				return -EPERM;
 			}
 		} else {
 			/* No action is required */
@@ -1159,7 +1159,7 @@ static int update_ripas(unsigned long *s2ttep, long level,
 			if (change_destroyed == CHANGE_DESTROYED) {
 				s2tte = s2tte_create_unassigned_empty();
 			} else {
-				return -1;
+				return -EPERM;
 			}
 		} else if (s2tte_is_assigned_ram(s2tte, level)) {
 			pa = s2tte_pa(s2tte, level);
@@ -1173,7 +1173,7 @@ static int update_ripas(unsigned long *s2ttep, long level,
 				/* TLBI is required */
 				ret = 1;
 			} else {
-				return -1;
+				return -EPERM;
 			}
 		} else {
 			/* No action is required */
