edfccbb652b4c884bb7994c950b680bd17032891
Mate Toth-Pal <mate.toth-pal@arm.com>
2023-10-27 17:05:01 +0200

    feat(cbmc): implement host_gtsi_delegate
    Add implementation to host_gtsi_[un]delegate to emulate Granule Transition
    Service changing granule PAS. This allows to reach 100% coverage with
    tb_rmi_granule_delegate.
    
    Change-Id: Ia9f0b21f508a9a015606f6a279078d829dd54e86
    Signed-off-by: Mate Toth-Pal <mate.toth-pal@arm.com>

--- plat/host/host_cbmc/src/host_harness.c
+++ plat/host/host_cbmc/src/host_harness.c
@@ -5,6 +5,7 @@
 
 #include <buffer.h>
 #include <host_harness.h>
+#include <tb_common.h>
 
 void *host_buffer_arch_map(unsigned int slot, unsigned long addr)
 {
@@ -20,10 +21,21 @@ void host_buffer_arch_unmap(void *buf)
 
 unsigned long host_gtsi_delegate(unsigned long addr)
 {
-	return 0;
+	if(get_granule_gpt_ns(addr)) {
+		set_granule_gpt_ns(addr, false);
+		return 0UL;
+	} else {
+		return 1UL;
+	}
 }
 
 unsigned long host_gtsi_undelegate(unsigned long addr)
 {
-	return 0;
+	if(get_granule_gpt_ns(addr)) {
+		return 1UL;
+	} else {
+		set_granule_gpt_ns(addr, true);
+		return 0UL;
+	}
 }
+
