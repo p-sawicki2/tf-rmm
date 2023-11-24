2fae32fe6acfb5db506c91d0a3aac608ee42b93b
Javier Almansa Sobrino <javier.almansasobrino@arm.com>
2023-11-24 18:28:50 +0000

    feat(s2tte): add support for FEAT_LPA2 to the s2tte library
    For simplicity, stage 2 translation always enables support for LPA2
    regardless of whether LPA2 is enabled on the running realm by the
    host.
    
    During realm creation, RMM verifies the IPA size and start level
    against LPA2 settings setup by the Host.
    
    This patch also checks that the memory banks specified in the
    Boot Manifest are within the platform address space.
    
    Signed-off-by: Javier Almansa Sobrino <javier.almansasobrino@arm.com>
    Change-Id: I2c42b7cb9ce40cd17a7426bafa15f0a2fbee0dac

--- lib/realm/src/s2tt_private_defs.h
+++ lib/realm/src/s2tt_private_defs.h
@@ -11,9 +11,10 @@
 
 /*
  * The maximum number of bits supported by the RMM for a stage 2 translation
- * output address (including stage 2 table entries).
+ * output address (including stage 2 table entries) with FEAT_LPA2 is 52 bits.
  */
-#define S2TTE_OA_BITS			48U
+#define S2TTE_OA_BITS_LPA2		52UL
+#define S2TTE_OA_BITS			48UL
 
 /*
  * The following constants for the mapping attributes (S2_TTE_MEMATTR_*)
@@ -35,19 +36,28 @@
 #define S2TTE_SH_OS			(2UL << S2TTE_SH_SHIFT)
 #define S2TTE_SH_IS			(3UL << S2TTE_SH_SHIFT)	/* Inner Shareable */
 
-/*
- * We set HCR_EL2.FWB So we set bit[4] to 1 and bits[3:2] to 2 and force
- * everyting to be Normal Write-Back
- */
-#define S2TTE_MEMATTR_FWB_NORMAL_WB	((1UL << 4) | (2UL << 2))
-
-#define S2TTE_ATTRS	(S2TTE_MEMATTR_FWB_NORMAL_WB | S2TTE_AP_RW |	\
-			S2TTE_SH_IS | S2TTE_AF)
+/* When FEAT_LPA2 is enabled, Shareability attributes are stored in VTCR_EL2 */
+#define S2TTE_ATTRS_LPA2	(S2TTE_MEMATTR_FWB_NORMAL_WB | S2TTE_AP_RW | \
+				 S2TTE_AF)
+#define S2TTE_ATTRS_LPA2_MASK	(S2TTE_MEMATTR_MASK | S2TTE_AP_MASK | S2TTE_AF)
+#define S2TTE_ATTRS		(S2TTE_ATTRS_LPA2 | S2TTE_SH_IS)
+#define S2TTE_ATTRS_MASK	(S2TTE_ATTRS_LPA2_MASK | S2TTE_SH_MASK)
 
 /* NS attributes set by the host */
-#define S2TTE_NS_ATTR_HOST_MASK 					\
-			(S2TTE_MEMATTR_MASK | S2TTE_AP_MASK | 		\
-			 S2TTE_SH_MASK)
+#define S2TTE_HOST_NS_ATTR_LPA2_MASK (S2TTE_MEMATTR_MASK | S2TTE_AP_MASK)
+#define S2TTE_HOST_NS_ATTR_MASK (S2TTE_HOST_NS_ATTR_LPA2_MASK | S2TTE_SH_MASK)
+
+/* Descriptor templantes */
+#define S2TTE_TABLE		S2TTE_L012_TABLE
+#define S2TTE_BLOCK		(S2TTE_ATTRS | S2TTE_L012_BLOCK)
+#define S2TTE_BLOCK_LPA2	(S2TTE_ATTRS_LPA2 | S2TTE_L012_BLOCK)
+#define S2TTE_PAGE		(S2TTE_ATTRS | S2TTE_L3_PAGE)
+#define S2TTE_PAGE_LPA2		(S2TTE_ATTRS_LPA2 | S2TTE_L3_PAGE)
+#define S2TTE_BLOCK_NS		(S2TTE_NS | S2TTE_XN |			\
+				 S2TTE_AF | S2TTE_L012_BLOCK)
+#define S2TTE_PAGE_NS		(S2TTE_NS | S2TTE_XN |			\
+				 S2TTE_AF | S2TTE_L3_PAGE)
+#define S2TTE_INVALID		S2TTE_Lx_INVALID
 
 /*
  * NS attributes not set by the host.
@@ -55,16 +65,24 @@
  */
 #define S2TTE_NS_ATTR_MASK	(S2TTE_AF | S2TTE_NS | S2TTE_XN)
 
-#define S2TTE_TABLE	S2TTE_L012_TABLE
-#define S2TTE_BLOCK	(S2TTE_ATTRS | S2TTE_L012_BLOCK)
-#define S2TTE_PAGE	(S2TTE_ATTRS | S2TTE_L3_PAGE)
-#define S2TTE_BLOCK_NS	(S2TTE_NS | S2TTE_XN | S2TTE_AF | S2TTE_L012_BLOCK)
-#define S2TTE_PAGE_NS	(S2TTE_NS | S2TTE_XN | S2TTE_AF | S2TTE_L3_PAGE)
-#define S2TTE_INVALID	S2TTE_Lx_INVALID
-
 /* Maximum number of concatenated tables for the start level */
 #define S2TTE_MAX_CONCAT_TABLES		(16U)
 
-#define NR_RTT_LEVELS	4
+/*
+ * When FEAT_LPA2 is enabled, the 2 MSB bits of the OA is not contiguous
+ * to the rest of the address in the TTE.
+ */
+#define S2TTE_OA_MSB_SHIFT	50U
+#define S2TTE_OA_MSB_WIDTH	2U
+
+/* Where the 2 MSB bits of the OA are stored in the TTE */
+#define S2TTE_MSB_SHIFT	8U
+#define S2TTE_MSB_WIDTH	S2TTE_OA_MSB_WIDTH
+#define S2TTE_MSB_MASK	MASK(S2TTE_MSB)
+
+#define NR_RTT_LEVELS			(RTT_PAGE_LEVEL -		\
+						RTT_MIN_STARTING_LEVEL + 1U)
+#define NR_RTT_LEVELS_LPA2		(RTT_PAGE_LEVEL -		\
+						RTT_MIN_STARTING_LEVEL_LPA2 + 1U)
 
 #endif /* S2TT_PRIVATE_DEFS */
