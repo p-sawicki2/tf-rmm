01ac0173dce7f08ab0c1cbdde313c8992dea0653
AlexeiFedorov <Alexei.Fedorov@arm.com>
2023-11-27 13:04:26 +0000

    chore(rmm): calculate RIM extension on passed data
    This patch removes 64KB 'rec_params_per_cpu' array of
    'rmi_rec_params' structures used for RMI_REC_CREATE
    extension of RIM. Calculation is performed on
    'struct rmi_rec_params' data passed by '*rec_params'
    parameter from the calling function after clearing
    non-relevant parts to 0. Unused bits of the structure
    SBZ according to the RMM Specification.
    
    Change-Id: I5a940606c63990c4c2a7e1f2a9929755e154a713
    Signed-off-by: AlexeiFedorov <Alexei.Fedorov@arm.com>

--- lib/measurement/src/rim.c
+++ lib/measurement/src/rim.c
@@ -3,14 +3,8 @@
  * SPDX-FileCopyrightText: Copyright TF-RMM Contributors.
  */
 
-#include <cpuid.h>
 #include <measurement.h>
 #include <string.h>
-
-/*
- * Allocate a dummy rec_params for copying relevant parameters for measurement
- */
-static struct rmi_rec_params rec_params_per_cpu[MAX_CPUS];
 
 void measurement_data_granule_measure(unsigned char rim_measurement[],
 				      enum hash_algo algorithm,
@@ -91,35 +85,29 @@ void measurement_rec_params_measure(unsigned char rim_measurement[],
 				    struct rmi_rec_params *rec_params)
 {
 	struct measurement_desc_rec measure_desc = {0};
-	struct rmi_rec_params *rec_params_measured =
-		&rec_params_per_cpu[my_cpuid()];
-
-	(void)memset(rec_params_measured, 0, sizeof(*rec_params_measured));
 
 	/*
-	 * Copy the relevant parts of the rmi_rec_params structure to be
-	 * measured
+	 * Clear non-relevant parts of the rmi_rec_params structure
+	 * for initial measurement. Unused bits of the structure SBZ
+	 * according to the Specification.
 	 */
-	rec_params_measured->pc = rec_params->pc;
-	rec_params_measured->flags = rec_params->flags;
-	(void)memcpy(rec_params_measured->gprs, rec_params->gprs,
-					sizeof(rec_params->gprs));
+	rec_params->mpidr = 0UL;
+	rec_params->num_aux = 0UL;
+	(void)memset(rec_params->aux, 0, sizeof(rec_params->aux));
 
 	/* Initialize the measurement descriptior structure */
 	measure_desc.desc_type = MEASURE_DESC_TYPE_REC;
 	measure_desc.len = sizeof(struct measurement_desc_rec);
 	(void)memcpy(measure_desc.rim, rim_measurement,
 					measurement_get_size(algorithm));
-
 	/*
 	 * Hash the REC params structure and store the result in the
 	 * measurement descriptor structure.
 	 */
 	measurement_hash_compute(algorithm,
-				rec_params_measured,
-				sizeof(*rec_params_measured),
+				rec_params,
+				sizeof(struct rmi_rec_params),
 				measure_desc.content);
-
 	/*
 	 * Hash the measurement descriptor structure; the result is the
 	 * updated RIM.
