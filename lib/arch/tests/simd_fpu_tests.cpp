2c773128f4c49200974008452155603e21f8b93c
Chuyue Luo <Chuyue.Luo@arm.com>
2023-12-08 14:51:43 +0000

    test(lib/arch): helpers for testing simd_context_switch()
    This patch adds two helper functions which allow us to test the
    simd_context_switch() function. These are required because progress in
    the function under test relies on the value of
    g_simd_state_saved[my_cpuid()]. This value cannot be modified directly
    by any external script, it can only be modified by calling
    simd_context_switch().
    
    The helpers set the value of g_simd_state_saved to a specified value by
    calling simd_context_init() with the correct arguments.
    
    The patch also includes a few unit tests demonstrating how these helpers
    are used.
    
    Signed-off-by: Chuyue Luo <Chuyue.Luo@arm.com>
    Change-Id: Ib0fa3f339dc9e019a10f08608190a884ba18a8c3

--- lib/arch/tests/simd_fpu_tests.cpp
+++ lib/arch/tests/simd_fpu_tests.cpp
@@ -26,6 +26,7 @@ TEST_GROUP(simd_fpu) {
 
 	TEST_TEARDOWN()
 	{
+		simd_test_helpers_teardown();
 	}
 };
 
@@ -117,4 +118,131 @@ TEST(simd_fpu, simd_context_init_TC3)
 				&test_simd_cfg);
 
 	CHECK_TRUE(ret == -1);
+}
+
+TEST(simd_fpu, simd_context_switch_TC1)
+{
+	struct simd_context test_simd_ctx = { .sflags = 0 };
+	simd_owner_t owner =
+			(simd_owner_t)test_helpers_get_rand_in_range(1UL, 2UL);
+	u_register_t expected_fpcr =
+				test_helpers_get_rand_in_range(0UL, UINT64_MAX);
+	u_register_t expected_fpsr =
+				test_helpers_get_rand_in_range(0UL, UINT64_MAX);
+
+	/******************************************************************
+	 * TEST CASE 1:
+	 *
+	 * Call simd_context_switch() with only a ctx_save, when
+	 * g_simd_state_saved[my_cpuid()] is false and the context is
+	 * valid. Expect the save to complete correctly.
+	 ******************************************************************/
+
+	/*
+	 * Need to ensure g_simd_state_saved[my_cpuid()] is set to false prior
+	 * to calling simd_context_switch(). This allows us to verify that the
+	 * save completes correctly when this is the case.
+	 */
+	simd_test_helpers_set_state_saved(false);
+
+	test_simd_ctx.sflags |= SIMD_SFLAG_INIT_DONE;
+	test_simd_ctx.sflags &= ~SIMD_SFLAG_SAVED;
+	test_simd_ctx.tflags &= ~SIMD_TFLAG_SVE;
+	test_simd_ctx.tflags &= ~SIMD_TFLAG_SME;
+	test_simd_ctx.owner = owner;
+
+	write_fpcr(expected_fpcr);
+	write_fpsr(expected_fpsr);
+
+	(void)simd_context_switch(&test_simd_ctx, NULL);
+
+	CHECK_TRUE(test_simd_ctx.ctl_status_regs.fpcr == expected_fpcr);
+	CHECK_TRUE(test_simd_ctx.ctl_status_regs.fpsr == expected_fpsr);
+
+	CHECK_TRUE(simd_is_state_saved());
+}
+
+ASSERT_TEST(simd_fpu, simd_context_switch_TC2)
+{
+	struct simd_context test_simd_ctx = { .sflags = 0 };
+	simd_owner_t owner =
+			(simd_owner_t)test_helpers_get_rand_in_range(1UL, 2UL);
+
+	/******************************************************************
+	 * TEST CASE 2:
+	 *
+	 * Call simd_context_switch() with only a ctx_save, when the
+	 * INIT_DONE bit for the context is not set. Expect an assertion to
+	 * fail.
+	 ******************************************************************/
+
+	simd_test_helpers_set_state_saved(false);
+
+	test_simd_ctx.sflags &= ~SIMD_SFLAG_INIT_DONE;
+	test_simd_ctx.sflags &= ~SIMD_SFLAG_SAVED;
+	test_simd_ctx.tflags &= ~SIMD_TFLAG_SVE;
+	test_simd_ctx.tflags &= ~SIMD_TFLAG_SME;
+	test_simd_ctx.owner = owner;
+
+	test_helpers_expect_assert_fail(true);
+
+	simd_context_switch(&test_simd_ctx, NULL);
+
+	test_helpers_fail_if_no_assert_failed();
+}
+
+ASSERT_TEST(simd_fpu, simd_context_switch_TC3)
+{
+	struct simd_context test_simd_ctx = { .sflags = 0 };
+	simd_owner_t owner =
+			(simd_owner_t)test_helpers_get_rand_in_range(1UL, 2UL);
+
+	/******************************************************************
+	 * TEST CASE 3:
+	 *
+	 * Call simd_context_switch() with only a ctx_save, when the SAVED
+	 * bit for the context is already set. Expect an assertion to fail.
+	 ******************************************************************/
+
+	simd_test_helpers_set_state_saved(false);
+
+	test_simd_ctx.sflags |= SIMD_SFLAG_INIT_DONE;
+	test_simd_ctx.sflags |= SIMD_SFLAG_SAVED;
+	test_simd_ctx.tflags &= ~SIMD_TFLAG_SVE;
+	test_simd_ctx.tflags &= ~SIMD_TFLAG_SME;
+	test_simd_ctx.owner = owner;
+
+	test_helpers_expect_assert_fail(true);
+
+	simd_context_switch(&test_simd_ctx, NULL);
+
+	test_helpers_fail_if_no_assert_failed();
+}
+
+ASSERT_TEST(simd_fpu, simd_context_switch_TC4)
+{
+	struct simd_context test_simd_ctx = { .sflags = 0 };
+	simd_owner_t owner =
+			(simd_owner_t)test_helpers_get_rand_in_range(1UL, 2UL);
+
+	/******************************************************************
+	 * TEST CASE 4:
+	 *
+	 * Call simd_context_switch() with only a ctx_save, and
+	 * g_simd_state_saved[my_cpuid()] set. Expect an assertion to fail.
+	 ******************************************************************/
+
+	simd_test_helpers_set_state_saved(true);
+
+	test_simd_ctx.sflags |= SIMD_SFLAG_INIT_DONE;
+	test_simd_ctx.sflags &= ~SIMD_SFLAG_SAVED;
+	test_simd_ctx.tflags &= ~SIMD_TFLAG_SVE;
+	test_simd_ctx.tflags &= ~SIMD_TFLAG_SME;
+	test_simd_ctx.owner = owner;
+
+	test_helpers_expect_assert_fail(true);
+
+	simd_context_switch(&test_simd_ctx, NULL);
+
+	test_helpers_fail_if_no_assert_failed();
 }
--- lib/arch/tests/simd_test_helpers.c
+++ lib/arch/tests/simd_test_helpers.c
@@ -3,13 +3,52 @@
  * SPDX-FileCopyrightText: Copyright TF-RMM Contributors.
  */
 
+#include <host_utils.h>
 #include <simd_test_helpers.h>
 #include <test_helpers.h>
+
+static void simd_test_helpers_setup_sysregs(void)
+{
+	int ret;
+
+	(void)host_util_set_default_sysreg_cb("fpcr", 0UL);
+	ret = host_util_set_default_sysreg_cb("fpsr", 0UL);
+
+	assert(ret == 0);
+}
 
 void simd_test_helpers_init(void)
 {
 	test_helpers_init();
 	test_helpers_rmm_start(true);
+	simd_test_helpers_setup_sysregs();
 	host_util_set_cpuid(0U);
 	test_helpers_expect_assert_fail(false);
+}
+
+void simd_test_helpers_teardown(void)
+{
+	host_util_zero_sysregs_and_cbs();
+}
+
+void simd_test_helpers_set_state_saved(bool state)
+{
+	if (simd_is_state_saved() != state) {
+		struct simd_context simd_ctx = { .sflags = 0 };
+
+		simd_ctx.sflags |= SIMD_SFLAG_INIT_DONE;
+		simd_ctx.tflags &= ~SIMD_TFLAG_SVE;
+		simd_ctx.tflags &= ~SIMD_TFLAG_SME;
+		simd_ctx.owner = SIMD_OWNER_REL1;
+
+		if (state) {
+			simd_ctx.sflags &= ~SIMD_SFLAG_SAVED;
+			simd_context_switch(&simd_ctx, NULL);
+		} else {
+			simd_ctx.sflags |= SIMD_SFLAG_SAVED;
+			simd_context_switch(NULL, &simd_ctx);
+		}
+
+		assert(simd_is_state_saved() == state);
+	}
 }
