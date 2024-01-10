9529bc48dffab854865dc064b51c0d2f219ed8a8
Mate Toth-Pal <mate.toth-pal@arm.com>
2024-01-10 11:49:58 +0100

    feat(tools/cbmc): add option to build with gcc
    This feature adds a build option that calls gcc with the file and define
    arguments that is used by the CBMC builds.
    
    This feature is useful when hunting bugs/adding features to the CBMC
    build, as it has more advanced error reporting.
    
    Change-Id: Icb2db2261bb0796c36e725eec0540d501025daa7
    Signed-off-by: Mate Toth-Pal <mate.toth-pal@arm.com>

--- plat/common/include/plat_common.h
+++ plat/common/include/plat_common.h
@@ -6,13 +6,33 @@
 #ifndef PLAT_COMMON_H
 #define PLAT_COMMON_H
 
+#ifdef CBMC
+#include <tb_common.h>
+#endif /* CBMC */
+
 /* Forward declaration */
 struct xlat_mmap_region;
 
+#ifndef CBMC
 int plat_cmn_setup(unsigned long x0, unsigned long x1,
 		   unsigned long x2, unsigned long x3,
 		   struct xlat_mmap_region *plat_regions,
 		   unsigned int nregions);
 int plat_cmn_warmboot_setup(void);
+#else /* CBMC */
+static inline int plat_cmn_setup(unsigned long x0, unsigned long x1,
+				 unsigned long x2, unsigned long x3,
+				 struct xlat_mmap_region *plat_regions,
+				 unsigned int nregions)
+{
+	ASSERT(false, "plat_cmn_setup");
+	return 0;
+}
+static inline int plat_cmn_warmboot_setup(void)
+{
+	ASSERT(false, "plat_cmn_warmboot_setup");
+	return 0;
+}
+#endif /* CBMC */
 
 #endif /* PLAT_COMMON_H */
