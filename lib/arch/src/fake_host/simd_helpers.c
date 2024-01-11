df3f03f83d4142a291bc73e18b03027e214378dc
Chuyue Luo <Chuyue.Luo@arm.com>
2024-01-11 09:46:05 +0000

    feat(lib/arch): add framework for testing SIMD
    This patch adds the framework for unit testing functions in simd.c. This
    includes functionality for registering callbacks for the helper
    functions in simd_helper.c.
    
    Signed-off-by: Chuyue Luo <Chuyue.Luo@arm.com>
    Change-Id: I763b73d2963c16f167fc0bc2667a7503472c7fc4

--- lib/arch/src/fake_host/simd_helpers.c
+++ lib/arch/src/fake_host/simd_helpers.c
@@ -4,15 +4,34 @@
  */
 
 #include <simd.h>
+#include <simd_callbacks.h>
+#include <string.h>
+
+uintptr_t callbacks[SIMD_CB_IDS];
+
+static uintptr_t get_callback(enum simd_cb_ids id)
+{
+	return callbacks[id];
+}
 
 void fpu_save_registers(struct fpu_regs *regs)
 {
-	(void)regs;
+	cb_fpu_save_restore cb =
+			       (cb_fpu_save_restore)get_callback(FPU_SAVE_REGS);
+
+	if (cb != NULL) {
+		cb(regs);
+	}
 }
 
 void fpu_restore_registers(struct fpu_regs *regs)
 {
-	(void)regs;
+	cb_fpu_save_restore cb =
+			       (cb_fpu_save_restore)get_callback(FPU_SAVE_REGS);
+
+	if (cb != NULL) {
+		cb(regs);
+	}
 }
 
 uint32_t sve_rdvl(void)
@@ -34,4 +53,18 @@ void sve_restore_vector_registers(struct sve_regs *regs, bool restore_ffr)
 
 void sve_clear_p_ffr_registers(void)
 {
+}
+
+/******************************************************************
+ * APIs for test callbacks
+ *****************************************************************/
+
+void simd_test_register_callback(enum simd_cb_ids id, union simd_cbs cb)
+{
+	callbacks[id] = (uintptr_t)cb.fpu_save_restore_regs;
+}
+
+void simd_test_unregister_callback(enum simd_cb_ids id)
+{
+	callbacks[id] = (uintptr_t)NULL;
 }
