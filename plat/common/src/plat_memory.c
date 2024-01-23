79e08c23ccd446771e315fcfe2efcc4429c69867
Sathyam Panda <sathyam.panda@arm.com>
2024-01-23 14:00:30 +0000

    plat(common): move to a platform-independent DRAM manager
    DRAM management in RMM is not platform specific, so it can be moved
    to plat/common.
    
    Signed-off-by: Sathyam Panda <sathyam.panda@arm.com>
    Signed-off-by: Harry Moulton <harry.moulton@arm.com>
    Change-Id: I92122d4a97c1b4780cf14de5d8f4a40fc2b6af15

--- plat/common/src/plat_memory.c
+++ plat/common/src/plat_memory.c
@@ -1,26 +1,26 @@
 /*
  * SPDX-License-Identifier: BSD-3-Clause
- * SPDX-FileCopyrightText: Copyright TF-RMM Contributors.
+ * SPDX-FileCopyrightText: Copyright TF-RMM Contributors
  */
 
 #include <arch_helpers.h>
 #include <assert.h>
-#include <fvp_dram.h>
-#include <rmm_el3_ifc.h>
+#include <plat_common.h>
 
 COMPILER_ASSERT(MAX_DRAM_NUM_BANKS == 2UL);
 
-void fvp_set_dram_layout(struct ns_dram_info *plat_dram)
+static struct plat_dram_layout dram;
+
+void plat_set_dram_layout(struct ns_dram_info *plat_dram)
 {
 	uint64_t num_banks, num_granules = 0UL;
 	struct ns_dram_bank *bank_ptr;
-	struct fvp_dram_layout *dram_ptr = fvp_get_dram_layout();
 
 	/* Number of banks */
 	num_banks = plat_dram->num_banks;
 	assert(num_banks <= MAX_DRAM_NUM_BANKS);
 
-	/* Pointer to dram_bank[] array */
+	/* Pointer to the dram_bank[] array */
 	bank_ptr = plat_dram->banks;
 
 	for (unsigned long i = 0UL; i < num_banks; i++) {
@@ -29,20 +29,25 @@ void fvp_set_dram_layout(struct ns_dram_info *plat_dram)
 		uintptr_t end = start + size - 1UL;
 
 		if (i == 1UL) {
-			/* Start granule index in bank 1 */
-			dram_ptr->idx_bank_1 = num_granules;
+			/* Start granule index at 1 */
+			dram.idx_bank_1 = num_granules;
 		}
 
 		/* Total number of granules */
 		num_granules += (size / GRANULE_SIZE);
 
-		dram_ptr->fvp_bank[i].start_addr = start;
-		dram_ptr->fvp_bank[i].end_addr = end;
+		dram.bank[i].start_addr = start;
+		dram.bank[i].end_addr = end;
 
 		bank_ptr++;
 	}
 
-	dram_ptr->num_granules = num_granules;
+	dram.num_granules = num_granules;
 
-	inv_dcache_range((uintptr_t)dram_ptr, sizeof(struct fvp_dram_layout));
+	inv_dcache_range((uintptr_t)&dram, sizeof(&dram));
+}
+
+struct plat_dram_layout *plat_get_dram_layout(void)
+{
+	return &dram;
 }
