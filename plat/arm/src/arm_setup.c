2931f8a947676d17654a1a5ef79951129f2d303d
Soby Mathew <soby.mathew@arm.com>
2024-02-27 16:08:42 +0000

    feat(plat/arm): Introduce the common arm platform
    This patch introduced the common arm platform which can be used
    by any compatible hardware. This allows reuse of code across multiple
    platforms and possibly a single binary which can run on these platforms.
    This depends on the MAX_CPU and RMM_MAX_GRANULES and RMM_MAX_SIZE can
    encompass the superset of the values.
    
    Signed-off-by: Harry Moulton <harry.moulton@arm.com>
    Signed-off-by: Soby Mathew <soby.mathew@arm.com>
    Change-Id: I69ceea101595f0e8cac81bd9e4d8754d2b1edbc3

--- plat/arm/src/arm_setup.c
+++ plat/arm/src/arm_setup.c
@@ -4,9 +4,8 @@
  */
 
 #include <arch_features.h>
+#include <arm_dram.h>
 #include <debug.h>
-#include <fvp_dram.h>
-#include <fvp_private.h>
 #include <pl011.h>
 #include <plat_common.h>
 #include <platform_api.h>
@@ -15,10 +14,7 @@
 #include <string.h>
 #include <xlat_tables.h>
 
-/* FVP UART Base address. */
-#define FVP_UART_ADDR	UL(0x1c0c0000)
-
-#define FVP_RMM_UART	MAP_REGION_FLAT(			\
+#define ARM_RMM_UART	MAP_REGION_FLAT(			\
 				0,			\
 				SZ_4K,				\
 				(MT_DEVICE | MT_RW | MT_REALM))
@@ -56,13 +52,15 @@ void plat_warmboot_setup(uint64_t x0, uint64_t x1, uint64_t x2, uint64_t x3)
 void plat_setup(uint64_t x0, uint64_t x1, uint64_t x2, uint64_t x3)
 {
 	int ret;
+	uintptr_t uart_base;
+	unsigned int clk_in_hz, baud_rate;
 	struct ns_dram_info *plat_dram;
 	struct console_list *csl_list;
 	struct console_info *console_ptr;
 
 	/* TBD Initialize UART for early log */
 	struct xlat_mmap_region plat_regions[] = {
-		FVP_RMM_UART,
+		ARM_RMM_UART,
 		{0}
 	};
 
@@ -86,10 +84,12 @@ void plat_setup(uint64_t x0, uint64_t x1, uint64_t x2, uint64_t x3)
 			rmm_el3_ifc_report_fail_to_el3(E_RMM_BOOT_UNKNOWN_ERROR);
 		}
 
-		uintptr_t uart_base = console_ptr->base;
+		uart_base = console_ptr->base;
+		clk_in_hz = (unsigned int) console_ptr->clk_in_hz;
+		baud_rate = (unsigned int) console_ptr->baud_rate;
 
 		/* RMM currently only supports one console */
-		ret = pl011_init(uart_base, console_ptr->clk_in_hz, console_ptr->baud_rate);
+		ret = pl011_init(uart_base, clk_in_hz, baud_rate);
 		if (ret != 0) {
 			rmm_el3_ifc_report_fail_to_el3(E_RMM_BOOT_UNKNOWN_ERROR);
 		}
@@ -118,8 +118,8 @@ void plat_setup(uint64_t x0, uint64_t x1, uint64_t x2, uint64_t x3)
 		rmm_el3_ifc_report_fail_to_el3(ret);
 	}
 
-	/* Set up FVP DRAM layout */
-	fvp_set_dram_layout(plat_dram);
+	/* Set up Arm DRAM layout */
+	arm_set_dram_layout(plat_dram);
 
 	plat_warmboot_setup(x0, x1, x2, x3);
 }
