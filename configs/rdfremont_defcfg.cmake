25916b8c04215b4727797596245cd2602d09c79b
Harry Moulton <harry.moulton@arm.com>
2024-03-05 12:01:24 +0000

    plat(rdfremont): introduce initial rdfremont platform support
    RD-Fremont Fixed Virtual Platform is an Arm Neoverse Reference Design
    platform with ARMv9 RME enabled CPUs and 2 DRAM banks. Add initial
    platform support by adding the required platform configuration file
    that includes RMM size and granule information.
    
    Signed-off-by: Harry Moulton <harry.moulton@arm.com>
    Change-Id: I2c7330fde0ac51ffd5d3100017268ba9246b4b4d

---
+++ configs/rdfremont_defcfg.cmake
@@ -0,0 +1,22 @@
+#
+# SPDX-License-Identifier: BSD-3-Clause
+# SPDX-FileCopyrightText: Copyright TF-RMM Contributors.
+#
+
+#
+# Set the RMM_PLATFORM variable to Cmake cache.
+#
+set(RMM_PLATFORM "arm" CACHE STRING "platform")
+arm_config_option_override(NAME RMM_TOOLCHAIN DEFAULT "gnu")
+
+#
+# Set RMM_MAX_SIZE for this platform (24MB)
+#
+arm_config_option_override(NAME RMM_MAX_SIZE DEFAULT 0x02400000)
+
+#
+# Maximum number of granules supported, enough to cover
+# (2GB - 64MB) of DRAM0 and 2GB of DRAM1. We overprovision
+# for the case that DT has not catered for the 64 MB carveout.
+#
+arm_config_option_override(NAME RMM_MAX_GRANULES DEFAULT 0xfc000)
