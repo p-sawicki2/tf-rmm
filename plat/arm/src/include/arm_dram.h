3cb7406350be5c84ce2df7d5eaada805cecc7903
Harry Moulton <harry.moulton@arm.com>
2024-03-07 16:33:59 +0000

    feat(common): improve dram bank lookup
    Next generation neoverse reference designs, such as RD-Fremont,
    can have more than two non-contiguous DRAM banks. On such platforms,
    these banks are spread across the system memory map.
    
    This change modifies the common platform granule handlers to more
    efficiently translate address-to-index and index-to-address on platforms
    with a large number of DRAM banks.
    
    A new config option named RMM_MAX_NUM_DRAM_BANK is introduced to define
    the maximum number of banks supported by a given platform.
    
    Signed-off-by: Harry Moulton <harry.moulton@arm.com>
    Change-Id: I6d5b02f8d3052d6b189f21ef48d1f684acb63b86

--- plat/arm/src/include/arm_dram.h
+++ plat/arm/src/include/arm_dram.h
@@ -10,19 +10,17 @@
 #include <stddef.h>
 #include <stdint.h>
 
-/* Maximum number of DRAM banks supported */
-#define MAX_DRAM_NUM_BANKS	2UL
-
 /* Arm runtime structures */
 struct arm_dram_bank {
-	uintptr_t start_addr;		/* start address */
-	uintptr_t end_addr;		/* end address */
+	uint64_t base;			/* bank base address */
+	uint64_t size;			/* size of this bank */
+	uint64_t cumulative_size;	/* combined size of all previous banks */
 };
 
 struct arm_dram_layout {
-	unsigned long idx_bank_1;	/* start granule index in bank 1 */
 	unsigned long num_granules;	/* number of granules */
-	struct arm_dram_bank arm_bank[MAX_DRAM_NUM_BANKS];
+	unsigned long num_banks;	/* number of dram banks */
+	struct arm_dram_bank bank[RMM_MAX_NUM_DRAM_BANKS];
 };
 
 void arm_set_dram_layout(struct ns_dram_info *plat_dram);
