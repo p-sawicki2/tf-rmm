54926cb3423d95166eee8b0289bced7e8fdaf390
Arunachalam Ganapathy <arunachalam.ganapathy@arm.com>
2024-04-10 20:55:40 +0100

    feat(da): add libspdm version 3.3.0
    Add libspdm project to RMM submodule. libspdm is a sample implementation
    that follows the DMTF SPDM specifications DSP0274 [1].
    
    libspdm project provides libraries that RMM imports as an interface
    library named rmm-lib-spdm_requester to implement SPMD requester.
    
    There are few patch files added in libspdm for below changes:
    - Compiles cryptlib_mbedtls library with Mbed TLS 3.4.0
    - Includes mbedtls headers, config file from external Mbed TLS
    
    [1] https://www.dmtf.org/dsp/DSP0274
    
    Signed-off-by: Arunachalam Ganapathy <arunachalam.ganapathy@arm.com>
    Change-Id: I977877de39d6fca65c4a6e03ed679221c8a4bb47

--- configs/mbedtls/mbedtls_config.h
+++ configs/mbedtls/mbedtls_config.h
@@ -35,6 +35,8 @@
 #define MBEDTLS_ASN1_WRITE_C
 
 #define MBEDTLS_PLATFORM_SNPRINTF_MACRO snprintf
+/* For snprintf function declaration */
+#include <stdio.h>
 
 #define MBEDTLS_BASE64_C
 #define MBEDTLS_BIGNUM_C
@@ -64,8 +66,45 @@
 /* Memory buffer allocator option */
 #define MBEDTLS_MEMORY_ALIGN_MULTIPLE	8
 
+/* Configs required by SPDM requester libs - start */
+#define MBEDTLS_OID_C
+#define MBEDTLS_RSA_C
+#define MBEDTLS_PKCS1_V15
+#define MBEDTLS_PKCS1_V21
+#define MBEDTLS_ALLOW_PRIVATE_ACCESS
+#define MBEDTLS_GENPRIME
+
+#define MBEDTLS_X509_USE_C
+#define MBEDTLS_X509_CRT_PARSE_C
+#define MBEDTLS_X509_CRL_PARSE_C
+#define MBEDTLS_X509_CSR_PARSE_C
+#define MBEDTLS_X509_CREATE_C
+#define MBEDTLS_X509_CSR_WRITE_C
+
+#define MBEDTLS_AES_C
+#define MBEDTLS_GCM_C
+
+#define MBEDTLS_CHACHA20_C
+#define MBEDTLS_POLY1305_C
+#define MBEDTLS_CHACHAPOLY_C
+
+#define MBEDTLS_ECDH_C
+#define MBEDTLS_DHM_C
+
+#define MBEDTLS_PK_C
+#define MBEDTLS_PK_PARSE_C
+#define MBEDTLS_PK_WRITE_C
+
+#define MBEDTLS_ECP_DP_SECP256R1_ENABLED
+#define MBEDTLS_ECP_DP_SECP384R1_ENABLED
+#define MBEDTLS_ECP_DP_SECP521R1_ENABLED
+
+/* todo: add #define MBEDTLS_PEM_PARSE_C */
+/* Configs required by SPDM requester libs - end */
+
 /* This is needed for size_t used below */
 #include <stddef.h>
+#include <limits.h>
 
 /*
  * Declare memory allocation primitives to be used by MbedTLS
