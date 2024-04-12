8d5cc4ac4c502ba82521ed2b0bfc6688021be0a9
Arunachalam Ganapathy <arunachalam.ganapathy@arm.com>
2024-04-12 11:52:18 +0100

    feat(lib/spdm_requester): import target test_crypt from libspdm
    test_crypt helps to verify RMM version of mbedtls used by libspdm for
    its cryptographic calls. This also indirectly verifies rmm-lib-allocator
    that manages mbedtls heap.
    
    With this change the libspdm test_crypt test cases can be run through
    RMM unittests framework when build with HOST_VARIANT=host_test.
    
    $ cd <build_path>
    $ ./<build_type>/rmm.elf -t spdm_requester.test_crypt
    
    Signed-off-by: Arunachalam Ganapathy <arunachalam.ganapathy@arm.com>
    Change-Id: I4d598c2d521ced54d7c9a141c7b9f2c519e7ce9f

---
+++ lib/spdm_requester/tests/spdm_requester_tests.cpp
@@ -0,0 +1,46 @@
+/*
+ * SPDX-License-Identifier: BSD-3-Clause
+ * SPDX-FileCopyrightText: Copyright TF-RMM Contributors.
+ */
+
+#include <CppUTest/CommandLineTestRunner.h>
+#include <CppUTest/TestHarness.h>
+
+extern "C"
+{
+	#include <mbedtls/memory_buffer_alloc.h>
+	#include <memory_alloc.h>
+	#include <sizes.h>
+
+	int test_crypt_main(void);
+
+	static struct buffer_alloc_ctx unittest_buffer_alloc_ctx;
+	static unsigned char mbedtls_heap[REC_HEAP_SIZE * 2]
+					__aligned(sizeof(unsigned long));
+}
+
+TEST_GROUP(spdm_requester)
+{
+	int rc;
+
+	TEST_SETUP() {
+		/* Assign buffer context for mbedtls heap */
+		rc = buffer_alloc_ctx_assign(&unittest_buffer_alloc_ctx);
+		CHECK_TRUE(rc == 0);
+
+		mbedtls_memory_buffer_alloc_init(mbedtls_heap,
+						 sizeof(mbedtls_heap));
+	}
+
+	TEST_TEARDOWN() {
+		buffer_alloc_ctx_unassign();
+	}
+};
+
+TEST(spdm_requester, test_crypt)
+{
+	int rc = 0;
+
+	rc = test_crypt_main();
+	CHECK_TRUE(rc == 0);
+}
