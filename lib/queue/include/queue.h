b4c3a800358ecbb494e58936576e1c5d61cee567
Raghu Krishnamurthy <raghupathyk@nvidia.com>
2024-04-25 12:59:54 -0700

    feat(lib/queue): Add simple circular buffer
    Add a simple circular buffer implementation that can have a power of 2
    elements capacity. The intent for this simple implementation is to be
    used for cases where queue space is fixed at compile time. The
    implementation itself is not multi-thread safe and the expectation is
    that a locking mechanism outside of this library is used before making
    any function call in this library. Therefore it is not meant for extreme
    performance sensitive paths.
    
    Change-Id: Ic2186dacea2cf2455fa635c9071f5f333494bb95
    Signed-off-by: Raghu Krishnamurthy <raghupathyk@nvidia.com>

---
+++ lib/queue/include/queue.h
@@ -0,0 +1,127 @@
+/*
+ * SPDX-License-Identifier: BSD-3-Clause
+ * SPDX-FileCopyrightText: Copyright TF-RMM Contributors.
+ */
+
+#ifndef QUEUE_H
+#define QUEUE_H
+
+#include <stdbool.h>
+
+/**
+ * This circular queue implementation provides a fixed-size buffer for storing
+ * elements of any type. It supports operations like push, pop, check if the
+ * queue is full or empty, and get the current size of the queue.
+ * The capacity of the queue is restricted to a power of 2 and the elements
+ * are stored in fixed-size slots.
+ *
+ * Note: This implementation does not provide built-in thread safety. In a
+ * multi-threaded environment, external locks should be used to ensure thread
+ * safety.
+ *
+ * Note: This implementation is not optimized for high performance scenarios.
+ *
+ * See CIRCULAR_Q_DECLARE macro for declaring and initializing a circular queue.
+ */
+
+struct circular_q {
+	bool initialized;
+	unsigned long read;
+	unsigned long length;
+	unsigned long capacity;
+	unsigned long element_size;
+	void *buffer;
+};
+
+struct circular_q_iter {
+	struct circular_q *q;
+	unsigned long index;
+	unsigned long remaining;
+};
+
+#define CIRCULAR_Q_DECLARE(name, num_elements, element_size_in) \
+		static char name##_buffer[(num_elements) * (element_size_in)];     \
+		static struct circular_q name = { \
+			.read = 0, \
+			.length = 0, \
+			.capacity = (num_elements), \
+			.element_size = (element_size_in), \
+			.buffer = (name##_buffer) \
+		}
+
+/*
+ * Initialize the circular queue. Checks for common errors in the queue
+ * such as invalid capacity, element size, or buffer pointer. The function
+ * returns 0 on success, and -EINVAL on failure. It also sets the initialized
+ * flag to true if the checks pass.
+ */
+int q_init_check(struct circular_q *q);
+
+/*
+ * Push an element into the queue. The function returns 0 on success, and
+ * -EINVAL on failure such as when the queue is full or when the queue is not
+ * initialized. It returns -EAGAIN if the queue is full.
+ * Note that the function is not thread-safe and an external lock should be
+ * used to ensure thread safety. The implementation also copies the data into
+ * the queue buffer as opposed to storing a reference to the data. The size
+ * parameter is used to ensure that the data pointer has enough space to store
+ * the element.
+ */
+int q_push(struct circular_q *q, void *data, unsigned long size);
+
+/*
+ * Pop an element from the queue. The function returns 0 on success, and
+ * -EINVAL on failure such as when the queue is empty or when the queue is not
+ * initialized. It returns -EAGAIN if the queue is empty. The function also
+ * copies the data from the queue buffer into the data pointer. The size
+ * parameter is used to ensure that the data pointer has enough space to store
+ * the element.
+ * Note that the function is not thread-safe and an external lock should be
+ * used to ensure thread safety.
+ */
+int q_pop(struct circular_q *q, unsigned long *data, unsigned long size);
+
+/*
+ * Check if the queue is full. The function returns true if the queue is full,
+ * and false otherwise. It also checks if the queue is initialized before
+ * performing the check.
+ */
+bool q_full(struct circular_q *q);
+
+/*
+ * Check if the queue is empty. The function returns true if the queue is empty,
+ * and false otherwise. It also checks if the queue is initialized before
+ * performing the check.
+ */
+bool q_empty(struct circular_q *q);
+
+/*
+ * Get the current size of the queue. The function returns the number of
+ * elements in the queue. It also checks if the queue is initialized before
+ * returning the size.
+ */
+unsigned long q_size(struct circular_q *q);
+
+/*
+ * Initialize an iterator for the queue. The function returns 0 on success, and
+ * -EINVAL on failure such as when the queue is not initialized. The iterator
+ * is used to iterate over the elements in the queue without modifying the
+ * queue itself.
+ * Note that the function is not thread-safe and an external lock should be
+ * used to ensure thread safety.
+ * When the iterator is used, it is expected that all queue modifications are
+ * restricted.
+ */
+int q_init_iterator(struct circular_q *q, struct circular_q_iter *iter);
+
+/*
+ * Get the next element from the queue using the iterator. The function returns
+ * a pointer to the next element in the queue. It returns NULL if there are no
+ * more elements in the queue. The iterator is used to keep track of the current
+ * position in the queue.
+ * Note that the function is not thread-safe and an external lock should be
+ * used to ensure thread safety.
+ */
+void *q_iter_get_next(struct circular_q_iter *iter);
+
+#endif /* BUFFER_H */
