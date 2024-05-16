0f0f649270792714e56238e62df9355d966ba9b8
Shale Xiong <shale.xiong@arm.com>
2024-05-16 14:20:59 +0100

    chore(fuzzing): create RMI fuzzer host platform using AFL++.
    The fuzzing is based on the host_build platform but it fetches the test
    scenarios from the input file generated by the fuzzer. In each fuzzing
    iteration, the fuzzer injects random bytes, i.e. to the fuzzing input,
    containing the machine state of RMM, that is, host memory
    `granules_buffer` and granules array `granules`. Given the arbitrary
    initial state of RMM, fuzzer generates, by default, ten RMI calls to
    RMM, where registers X0 to X6 contains random values.
    
    More details can be found in
    `docs/resources/application-notes/fuzzing.rst`.
    
    Signed-off-by: Shale Xiong <shale.xiong@arm.com>
    Signed-off-by: Imre Kis <imre.kis@arm.com>
    Signed-off-by: Mate Toth-Pal <mate.toth-pal@arm.com>
    Change-Id: I75c725148c619a4c7d4b9b5a327df537277a075b

---
+++ tools/fuzz/python/parser.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# SPDX-License-Identifier: BSD-3-Clause
+# SPDX-FileCopyrightText: Copyright TF-RMM Contributors.
+
+"""Parse the generated input or any input caught by fuzzing"""
+
+if __name__ == "__main__":
+
+    from data_type import RMMFuzzingInput
+    import sys
+
+    with open(sys.argv[1], "rb") as file:
+        fuzzing_input = RMMFuzzingInput.from_bytes(file.read())
+        print(fuzzing_input)
