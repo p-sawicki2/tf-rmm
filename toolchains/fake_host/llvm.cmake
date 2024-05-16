1bf6466bac54a4ce7e1983cb677c0eebc7932448
Shale Xiong <shale.xiong@arm.com>
2024-05-16 14:20:59 +0100

    chore(fuzzing): create RMI fuzzer host platform using AFL++.
    The fuzzing is based on the host_build platform but it fetches the test
    scenarios from the input file generated by the fuzzer. In each fuzzing
    iteration, the fuzzer injects random bytes, i.e. to the fuzzing input,
    containing the machine state of RMM, that is, host memory
    `granules_buffer` and granules array `granules`. Given the arbitrary
    initial state of RMM, fuzzer generates, by default, ten RMI calls to
    RMM, where registers X0 to X6 contains random values.
    
    More details can be found in
    `docs/resources/application-notes/fuzzing.rst`.
    
    Signed-off-by: Shale Xiong <shale.xiong@arm.com>
    Signed-off-by: Imre Kis <imre.kis@arm.com>
    Signed-off-by: Mate Toth-Pal <mate.toth-pal@arm.com>
    Change-Id: I75c725148c619a4c7d4b9b5a327df537277a075b

--- toolchains/fake_host/llvm.cmake
+++ toolchains/fake_host/llvm.cmake
@@ -7,10 +7,33 @@ include_guard()
 
 include(${CMAKE_CURRENT_LIST_DIR}/common_fake_host.cmake)
 
+# When running fuzzing, use the afl custom compiler.
+if(HOST_VARIANT STREQUAL "host_fuzz")
+# Try to find the afl-clang-lto, 
+# which has to the best fuzzing performance
+find_program(CMAKE_C_COMPILER
+    NAMES "afl-clang-lto"
+    DOC "Path to afl-clang-lto if exists.")
+    if(${CMAKE_C_COMPILER})
+    # find alf-clang-lto, then also need to set linker
+    find_program(CMAKE_LINKER
+        NAMES "afl-ld-lto"
+        DOC "Path to afl-ld-lto if exists."
+        REQUIRED)
+    else()
+    #cannot find afl-clang-lto, full back to afl-clang-fast
+    find_program(CMAKE_C_COMPILER
+        NAMES "afl-clang-fast"
+        DOC "Path to afl-clang-lto if exists."
+        REQUIRED)
+    endif()
+else()
 find_program(CMAKE_C_COMPILER
     NAMES "clang"
     DOC "Path to clang."
     REQUIRED)
+endif()
+
 
 find_program(CMAKE_CXX_COMPILER
     NAMES "clang++"
