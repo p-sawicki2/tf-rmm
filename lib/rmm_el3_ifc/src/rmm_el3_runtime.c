ceb00ee5835a1ad8703dc0b7b55d88665ddaceff
Raghu Krishnamurthy <raghupathyk@nvidia.com>
2024-05-23 17:48:42 -0700

    feat(lib/rmm_el3_ifc): support for HES communication
    Add support functions to be able to talk to HES via EL3.
    
    Change-Id: I801ac4ab9cb232e8e8a5a54ec1238048f4285f51
    Signed-off-by: Raghu Krishnamurthy <raghupathyk@nvidia.com>

--- lib/rmm_el3_ifc/src/rmm_el3_runtime.c
+++ lib/rmm_el3_ifc/src/rmm_el3_runtime.c
@@ -34,14 +34,8 @@ void rmm_el3_ifc_release_shared_buf(void)
 	spinlock_release(&shared_area_lock);
 }
 
-/*
- * Get the realm attestation key to sign the realm attestation token. It is
- * expected that only the private key is retrieved in raw format.
- */
-int rmm_el3_ifc_get_realm_attest_key(uintptr_t buf, size_t buflen,
-					size_t *len, unsigned int crv)
+static unsigned long get_buffer_pa(uintptr_t buf, size_t buflen)
 {
-	struct smc_result smc_res;
 	unsigned long buffer_pa;
 	unsigned long offset = buf - rmm_shared_buffer_start_va;
 
@@ -50,8 +44,20 @@ int rmm_el3_ifc_get_realm_attest_key(uintptr_t buf, size_t buflen,
 
 	buffer_pa = (unsigned long)rmm_el3_ifc_get_shared_buf_pa() + offset;
 
+	return buffer_pa;
+}
+
+/*
+ * Get the realm attestation key to sign the realm attestation token. It is
+ * expected that only the private key is retrieved in raw format.
+ */
+int rmm_el3_ifc_get_realm_attest_key(uintptr_t buf, size_t buflen,
+					size_t *len, unsigned int crv)
+{
+	struct smc_result smc_res;
+
 	monitor_call_with_res(SMC_RMM_GET_REALM_ATTEST_KEY,
-			      buffer_pa,
+			      get_buffer_pa(buf, buflen),
 			      buflen,
 			      crv, 0UL, 0UL, 0UL, &smc_res);
 
@@ -76,17 +82,10 @@ int rmm_el3_ifc_get_platform_token(uintptr_t buf, size_t buflen,
 					size_t *len, size_t hash_size)
 {
 	struct smc_result smc_res;
-	unsigned long buffer_pa;
-	unsigned long offset = buf - rmm_shared_buffer_start_va;
-
-	assert((offset + buflen) <= rmm_el3_ifc_get_shared_buf_size());
-	assert((buf & ~PAGE_SIZE_MASK) == rmm_shared_buffer_start_va);
-
-	buffer_pa = (unsigned long)rmm_el3_ifc_get_shared_buf_pa() + offset;
 	/* Get the available space on the buffer after the offset */
 
 	monitor_call_with_res(SMC_RMM_GET_PLAT_TOKEN,
-			      buffer_pa,
+			      get_buffer_pa(buf, buflen),
 			      buflen,
 			      hash_size,
 			      0UL, 0UL, 0UL, &smc_res);
@@ -95,6 +94,58 @@ int rmm_el3_ifc_get_platform_token(uintptr_t buf, size_t buflen,
 	if (smc_res.x[0] != 0UL) {
 		ERROR("Failed to get platform token x0 = 0x%lx\n",
 				smc_res.x[0]);
+		return (int)smc_res.x[0];
+	}
+
+	*len = smc_res.x[1];
+
+	return 0;
+}
+
+/*
+ * Push an attestation request to the HES.
+ * The caller must have already populated the request in the shared buffer.
+ * The push operation may fail if EL3 does not have enough queue space or if
+ * the HES is not ready to accept the request.
+ */
+int rmm_el3_ifc_push_hes_request(uintptr_t buf, size_t buflen)
+{
+	struct smc_result smc_res;
+
+	monitor_call_with_res(SMC_RMM_HES_PUSH_ATTEST_REQ,
+			      get_buffer_pa(buf, buflen),
+			      buflen,
+			      0UL,
+			      0UL, 0UL, 0UL, &smc_res);
+
+	/* coverity[uninit_use:SUPPRESS] */
+	if (smc_res.x[0] != 0UL) {
+		VERBOSE("Failed to push attest req to HES x0 = 0x%lx\n",
+		      smc_res.x[0]);
+		return (int)smc_res.x[0];
+	}
+
+	return 0;
+}
+
+/*
+ * Pull an attestation response from the HES. The pull operation may fail if
+ * the HES has not yet provided a response.
+ */
+int rmm_el3_ifc_pull_hes_response(uintptr_t buf, size_t buflen,
+					size_t *len)
+{
+	struct smc_result smc_res;
+
+	monitor_call_with_res(SMC_RMM_HES_PULL_ATTEST_RESP,
+			      get_buffer_pa(buf, buflen),
+			      buflen,
+			      0UL, 0UL, 0UL, 0UL, &smc_res);
+
+	/* coverity[uninit_use:SUPPRESS] */
+	if (smc_res.x[0] != 0UL) {
+		VERBOSE("Failed to get attestation response x0 = 0x%lx\n",
+		      smc_res.x[0]);
 		return (int)smc_res.x[0];
 	}
 
