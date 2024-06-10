8718dc23517042e68d6a0bc110b8eada74d35e25
Raghu Krishnamurthy <raghupathyk@nvidia.com>
2024-06-10 15:35:41 -0700

    feat(runtime/core): add support for EL3 attestation queue
    This patch adds code to enable the use of a EL3 queue to queue
    attestation requests. When the RMM uses the t_cose adapter to sign
    attestation requests using EL3, the added code initializes call backs to
    take the CBOR encoded requests and hashes to be signed and enqueues it
    in EL3.
    The code added also provides functions that can be called to pull
    responses from EL3.
    
    Change-Id: Icbc1139f9c5fee5e1a229a964fdda291a332a8b8
    Signed-off-by: Raghu Krishnamurthy <raghupathyk@nvidia.com>

---
+++ runtime/include/el3_token_sign_queue.h
@@ -0,0 +1,27 @@
+/*
+ * SPDX-License-Identifier: BSD-3-Clause
+ * SPDX-FileCopyrightText: Copyright TF-RMM Contributors.
+ */
+#ifndef EL3_TOKEN_SIGN_H
+#define EL3_TOKEN_SIGN_H
+
+#include <stddef.h>
+#include <stdint.h>
+
+/*
+ * Initialize the EL3 token signing module. Also initialize callbacks
+ * in the t_cose library. The function returns 0 on success, and -EINVAL
+ * on failure.
+ */
+int el3_token_sign_queue_init(void);
+
+/*
+ * Pull responses to attestation signing requests from EL3. This function also
+ * checks consistency of the response and writes the response to the
+ * appropriate REC. Note that the REC is mapped at a different high-va slot
+ * than it normally is, to ensure we dont overwrite the REC that may be
+ * currently live when this function is called.
+ */
+void el3_token_sign_pull_response_from_el3(void);
+
+#endif /* EL3_TOKEN_SIGN_H */
