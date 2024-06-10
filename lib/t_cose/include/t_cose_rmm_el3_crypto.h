0d47ba161a2a7048c61ad3ba94dbe84575c4ad85
Raghu Krishnamurthy <raghupathyk@nvidia.com>
2024-06-10 13:25:19 -0700

    feat(lib/t_cose): EL3 based crypto
    Add a t_cose crypto adapter that is used by the RMM to hook crypto and
    redirect it to the EL3. By default, the RMM uses the PSA crypto adapter,
    but in order to support delegated attestation where the RMM does not
    have access to the attestation private key, we need the ability to
    redirect signing requests to the EL3 instead of PSA crypto. EL3 may then
    handle the request itself or forward to an agent like HES.
    During the redirect, the adapter queues attestation requests into a
    global EL3 queue via registered callback during library initialization.
    The main idea is to patch the psa crypto adapter in t_cose so that it
    forwards the sign call(s) if RMM_ATTESTATION_USE_EL3 is defined.
    As not all the functions that use the crypto_context are redirected (and
    even if they were, new functions can be added by upstream t_cose) this
    patch need to make sure that casting the crypto_context to
    t_cose_psa_crypto_context still works. So t_cose_rmm_el3_ctx is updated
    to have t_cose_psa_crypto_context as its first member.
    
    Change-Id: Ie9fefdfa3297e05406d7c06f82d9550c5c2d2474
    Signed-off-by: Mate Toth-Pal <mate.toth-pal@arm.com>
    Signed-off-by: Raghu Krishnamurthy <raghupathyk@nvidia.com>

---
+++ lib/t_cose/include/t_cose_rmm_el3_crypto.h
@@ -0,0 +1,50 @@
+/*
+ * SPDX-License-Identifier: BSD-3-Clause
+ * SPDX-FileCopyrightText: Copyright TF-RMM Contributors.
+ */
+
+#ifndef T_COSE_RMM_HES_CRYPTO_H
+#define T_COSE_RMM_HES_CRYPTO_H
+
+#include <psa/crypto.h>
+#include <spinlock.h>
+#include <t_cose_crypto.h>
+#include <t_cose_psa_crypto.h>
+#include <utils_def.h>
+
+struct t_cose_rmm_el3_ctx {
+
+	/*
+	 * Some of the operations in the crypto adaption layer might use the
+	 * crypto context. Make sure that casting this context to
+	 * `struct t_cose_psa_crypto_context` by them is safe.
+	 */
+	struct t_cose_psa_crypto_context psa_crypto_context;
+
+	spinlock_t lock;
+	struct {
+		/* cppcheck-suppress misra-c2012-6.1 */
+		bool is_req_queued : 1U;
+		/* cppcheck-suppress misra-c2012-6.1 */
+		bool is_sig_valid : 1U;
+		/* cppcheck-suppress misra-c2012-6.1 */
+		bool is_el3_err : 1U;
+		psa_algorithm_t alg_id;
+		size_t hash_len;
+		uintptr_t rec_granule;
+		uint64_t req_ticket;
+		uint16_t sig_len;
+		uint8_t sig_buffer[512];
+		uint8_t c_buffer_for_tbs_hash[T_COSE_CRYPTO_MAX_HASH_SIZE];
+	} state;
+};
+COMPILER_ASSERT_NO_CBMC(U(offsetof(struct t_cose_rmm_el3_ctx, psa_crypto_context)) == 0U);
+
+typedef bool (*t_cose_crypto_el3_enqueue_t)(struct t_cose_rmm_el3_ctx *el3_ctx,
+						uint64_t *ticket);
+
+void t_cose_crypto_el3_enqueue_cb_init(t_cose_crypto_el3_enqueue_t enqueue);
+void t_cose_crypto_el3_ctx_init(struct t_cose_rmm_el3_ctx *el3_ctx,
+				uintptr_t granule_addr);
+
+#endif /* T_COSE_RMM_HES_CRYPTO_H */
