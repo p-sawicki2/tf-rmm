428e1242113cc20461cdc3bfa595215ef3476366
Raghu Krishnamurthy <raghupathyk@nvidia.com>
2024-06-10 13:25:19 -0700

    feat(lib/t_cose): EL3 based crypto
    Add a t_cose crypto adapter that is used by the RMM to hook crypto and
    redirect it to the EL3. By default, the RMM uses the PSA crypto adapter,
    but in order to support delegated attestation where the RMM does not
    have access to the attestation private key, we need the ability to
    redirect signing requests to the EL3 instead of PSA crypto. EL3 may then
    handle the request itself or forward to an agent like HES.
    During the redirect, the adapter queues attestation requests into a
    global EL3 queue via registered callback during library initialization.
    The main idea is to patch the psa crypto adapter in t_cose so that it
    forwards the sign call(s) if RMM_ATTESTATION_USE_EL3 is defined.
    As not all the functions that use the crypto_context are redirected (and
    even if they were, new functions can be added by upstream t_cose) this
    patch need to make sure that casting the crypto_context to
    t_cose_psa_crypto_context still works. So t_cose_rmm_el3_ctx is updated
    to have t_cose_psa_crypto_context as its first member.
    
    Change-Id: Ie9fefdfa3297e05406d7c06f82d9550c5c2d2474
    Signed-off-by: Mate Toth-Pal <mate.toth-pal@arm.com>
    Signed-off-by: Raghu Krishnamurthy <raghupathyk@nvidia.com>

---
+++ lib/t_cose/src/t_cose_rmm_el3_crypto.c
@@ -0,0 +1,157 @@
+/*
+ * SPDX-License-Identifier: BSD-3-Clause
+ * SPDX-FileCopyrightText: Copyright TF-RMM Contributors.
+ */
+
+/*
+ * This file contains alternative implementations of the signing related
+ * functions defined in the t_cose crypto adaption layer.
+ * Instead of calling the PSA crypto interface, these implementations use the
+ * HES to perform the signing operation.
+ *
+ * These functions are called from the patched
+ * ext/t_cose/crypto_adapters/t_cose_psa_crypto.c file.
+ *
+ * For signing, we only use restartable signing since it allows returning
+ * to the caller without completing signing, which is required for
+ * offloading signing operations HES asynchronously.
+ */
+#include <assert.h>
+#include <memory.h>
+#include <t_cose_crypto.h> /* The interface this implements */
+#include <t_cose_rmm_el3_crypto.h>
+#include <t_cose_util.h>
+
+static t_cose_crypto_el3_enqueue_t t_cose_crypto_el3_enqueue;
+static bool lib_initialized;
+
+static void
+t_cose_crypto_init_el3_ctx_crypto(struct t_cose_rmm_el3_ctx *el3_ctx_locked,
+				  int32_t cose_algorithm_id,
+				  struct q_useful_buf_c hash_to_sign,
+				  struct q_useful_buf signature_buffer)
+{
+	/* Assumes lock is held for context */
+	el3_ctx_locked->state.alg_id = cose_alg_id_to_psa_alg_id(cose_algorithm_id);
+	el3_ctx_locked->state.sig_len = signature_buffer.len;
+	el3_ctx_locked->state.hash_len = hash_to_sign.len;
+	(void)memcpy(el3_ctx_locked->state.c_buffer_for_tbs_hash, hash_to_sign.ptr,
+	       hash_to_sign.len);
+}
+
+void t_cose_crypto_el3_enqueue_cb_init(t_cose_crypto_el3_enqueue_t enqueue)
+{
+	t_cose_crypto_el3_enqueue = enqueue;
+	lib_initialized = true;
+}
+
+void t_cose_crypto_el3_ctx_init(struct t_cose_rmm_el3_ctx *el3_ctx,
+				uintptr_t granule_addr)
+{
+	assert(lib_initialized);
+
+	spinlock_acquire(&el3_ctx->lock);
+	(void)memset(&el3_ctx->state, 0, sizeof(el3_ctx->state));
+	el3_ctx->state.is_req_queued = false;
+	el3_ctx->state.is_sig_valid = false;
+	el3_ctx->state.is_el3_err = false;
+	el3_ctx->state.rec_granule = granule_addr;
+	spinlock_release(&el3_ctx->lock);
+}
+
+/*
+ * See documentation in t_cose_crypto.h
+ */
+enum t_cose_err_t t_cose_crypto_sign_restart_el3(
+	bool started, int32_t cose_algorithm_id, struct t_cose_key signing_key,
+	void *crypto_context, struct q_useful_buf_c hash_to_sign,
+	struct q_useful_buf signature_buffer, struct q_useful_buf_c *signature)
+{
+	enum t_cose_err_t return_value;
+	struct t_cose_rmm_el3_ctx *el3_crypto_context;
+	psa_algorithm_t psa_alg_id;
+
+	(void)signing_key;
+
+	assert(lib_initialized);
+
+	psa_alg_id = cose_alg_id_to_psa_alg_id(cose_algorithm_id);
+	if (!PSA_ALG_IS_ECDSA(psa_alg_id) && !PSA_ALG_IS_RSA_PSS(psa_alg_id)) {
+		return_value = T_COSE_ERR_UNSUPPORTED_SIGNING_ALG;
+		goto done;
+	}
+
+	if (!crypto_context) {
+		return_value = T_COSE_ERR_FAIL;
+		goto done;
+	}
+
+	el3_crypto_context = (struct t_cose_rmm_el3_ctx *)crypto_context;
+
+	/*
+	 * Since the response corresponding to this REC can be updated by
+	 * another REC we need to protect the below from concurrent access.
+	 */
+	spinlock_acquire(&el3_crypto_context->lock);
+	if (!started) {
+		if (hash_to_sign.len >
+		    sizeof(el3_crypto_context->state.c_buffer_for_tbs_hash)) {
+			return_value = T_COSE_ERR_FAIL;
+
+			goto release;
+		}
+		t_cose_crypto_init_el3_ctx_crypto(el3_crypto_context,
+						  cose_algorithm_id,
+						  hash_to_sign,
+						  signature_buffer);
+	}
+
+	if (!el3_crypto_context->state.is_req_queued) {
+		if (t_cose_crypto_el3_enqueue(el3_crypto_context,
+			&el3_crypto_context->state.req_ticket)) {
+			el3_crypto_context->state.is_req_queued = true;
+		}
+	}
+
+	/*
+	 * Responses for this request are pulled outside the current function
+	 */
+	if (el3_crypto_context->state.is_el3_err) {
+		return_value = T_COSE_ERR_FAIL;
+		goto release;
+	}
+
+	return_value = T_COSE_ERR_SIG_IN_PROGRESS;
+
+	if (el3_crypto_context->state.is_sig_valid) {
+		assert(el3_crypto_context->state.is_req_queued == true);
+		assert(el3_crypto_context->state.sig_len <= signature_buffer.len);
+		(void)memcpy(signature_buffer.ptr, el3_crypto_context->state.sig_buffer,
+		       el3_crypto_context->state.sig_len);
+		signature->ptr = signature_buffer.ptr;
+		signature->len = el3_crypto_context->state.sig_len;
+		return_value = T_COSE_SUCCESS;
+	}
+
+release:
+	spinlock_release(&el3_crypto_context->lock);
+done:
+	return return_value;
+}
+
+enum t_cose_err_t t_cose_crypto_sign_el3(int32_t cose_algorithm_id,
+					 struct t_cose_key signing_key,
+					 void *crypto_context,
+					 struct q_useful_buf_c hash_to_sign,
+					 struct q_useful_buf signature_buffer,
+					 struct q_useful_buf_c *signature)
+{
+	(void)cose_algorithm_id;
+	(void)signing_key;
+	(void)crypto_context;
+	(void)hash_to_sign;
+	(void)signature_buffer;
+	(void)signature;
+
+	return T_COSE_ERR_UNSUPPORTED_SIGNING_ALG;
+}
