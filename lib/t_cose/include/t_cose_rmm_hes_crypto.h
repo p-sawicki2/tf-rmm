325de22370c80dc3303821035d7d919981bc77c9
Raghu Krishnamurthy <raghupathyk@nvidia.com>
2024-06-10 13:25:19 -0700

    feat(lib/t_cose): HES based crypto
    Add a t_cose crypto adapter that is used by the RMM to hook crypto and
    redirect it to the HES via a queue. By default, the RMM uses the PSA
    crypto adapter, but in order to support delegated attestation where the
    RMM does not have access to the attestation private key, we need the
    ability to redirect signing requests to the HES instead of PSA crypto.
    During the redirect, the adapter queues attestation requests into a
    global RMM queue. On all RMM entries, the RMM will check the request
    queue and send it to HES via EL3, and also checks for a response and
    writes it to REC.
    
    Change-Id: Ie9fefdfa3297e05406d7c06f82d9550c5c2d2474
    Signed-off-by: Raghu Krishnamurthy <raghupathyk@nvidia.com>

---
+++ lib/t_cose/include/t_cose_rmm_hes_crypto.h
@@ -0,0 +1,45 @@
+/*
+ * SPDX-License-Identifier: BSD-3-Clause
+ * SPDX-FileCopyrightText: Copyright TF-RMM Contributors.
+ */
+
+#ifndef T_COSE_RMM_HES_CRYPTO_H
+#define T_COSE_RMM_HES_CRYPTO_H
+
+#include <mbedtls/hkdf.h>
+#include <mbedtls/md.h>
+#include <psa/crypto.h>
+#include <spinlock.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <t_cose_crypto.h>
+#include <utils_def.h>
+
+struct t_cose_rmm_hes_ctx {
+	spinlock_t lock;
+	struct {
+		/* cppcheck-suppress misra-c2012-6.1 */
+		bool is_req_queued : 1U;
+		/* cppcheck-suppress misra-c2012-6.1 */
+		bool is_sig_valid : 1U;
+		/* cppcheck-suppress misra-c2012-6.1 */
+		bool is_hes_err : 1U;
+		psa_algorithm_t alg_id;
+		size_t hash_len;
+		uintptr_t rec_granule;
+		uint64_t req_ticket;
+		uint16_t sig_len;
+		uint8_t sig_buffer[512];
+		uint8_t c_buffer_for_tbs_hash[T_COSE_CRYPTO_MAX_HASH_SIZE];
+	} state;
+};
+
+typedef bool (*t_cose_crypto_hes_enqueue_t)(struct t_cose_rmm_hes_ctx *hes_ctx,
+						uint64_t *ticket);
+
+void t_cose_crypto_hes_init(t_cose_crypto_hes_enqueue_t enqueue);
+void t_cose_crypto_hes_ctx_init(struct t_cose_rmm_hes_ctx *hes_ctx,
+				uintptr_t granule_addr);
+
+#endif /* T_COSE_RMM_HES_CRYPTO_H */
