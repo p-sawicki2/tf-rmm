1fae3e0bceb0ff298410509cbdee0ac977e263ba
Raghu Krishnamurthy <raghupathyk@nvidia.com>
2024-06-18 16:30:50 -0700

    refactor(lib/rmm_el3_ifc): clean up repeated code
    Refactor code to eliminate repeated code.
    
    Change-Id: I7cb6b1517e2f9310ae368b6110adb325abd23808
    Signed-off-by: Raghu Krishnamurthy <raghupathyk@nvidia.com>

--- lib/rmm_el3_ifc/src/rmm_el3_runtime.c
+++ lib/rmm_el3_ifc/src/rmm_el3_runtime.c
@@ -34,14 +34,8 @@ void rmm_el3_ifc_release_shared_buf(void)
 	spinlock_release(&shared_area_lock);
 }
 
-/*
- * Get the realm attestation key to sign the realm attestation token. It is
- * expected that only the private key is retrieved in raw format.
- */
-int rmm_el3_ifc_get_realm_attest_key(uintptr_t buf, size_t buflen,
-					size_t *len, unsigned int crv)
+static unsigned long get_buffer_pa(uintptr_t buf, size_t buflen)
 {
-	struct smc_result smc_res;
 	unsigned long buffer_pa;
 	unsigned long offset = buf - rmm_shared_buffer_start_va;
 
@@ -50,8 +44,20 @@ int rmm_el3_ifc_get_realm_attest_key(uintptr_t buf, size_t buflen,
 
 	buffer_pa = (unsigned long)rmm_el3_ifc_get_shared_buf_pa() + offset;
 
+	return buffer_pa;
+}
+
+/*
+ * Get the realm attestation key to sign the realm attestation token. It is
+ * expected that only the private key is retrieved in raw format.
+ */
+int rmm_el3_ifc_get_realm_attest_key(uintptr_t buf, size_t buflen,
+					size_t *len, unsigned int crv)
+{
+	struct smc_result smc_res;
+
 	monitor_call_with_res(SMC_RMM_GET_REALM_ATTEST_KEY,
-			      buffer_pa,
+			      get_buffer_pa(buf, buflen),
 			      buflen,
 			      crv, 0UL, 0UL, 0UL, &smc_res);
 
@@ -76,17 +82,10 @@ int rmm_el3_ifc_get_platform_token(uintptr_t buf, size_t buflen,
 					size_t *len, size_t hash_size)
 {
 	struct smc_result smc_res;
-	unsigned long buffer_pa;
-	unsigned long offset = buf - rmm_shared_buffer_start_va;
-
-	assert((offset + buflen) <= rmm_el3_ifc_get_shared_buf_size());
-	assert((buf & ~PAGE_SIZE_MASK) == rmm_shared_buffer_start_va);
-
-	buffer_pa = (unsigned long)rmm_el3_ifc_get_shared_buf_pa() + offset;
 	/* Get the available space on the buffer after the offset */
 
 	monitor_call_with_res(SMC_RMM_GET_PLAT_TOKEN,
-			      buffer_pa,
+			      get_buffer_pa(buf, buflen),
 			      buflen,
 			      hash_size,
 			      0UL, 0UL, 0UL, &smc_res);
@@ -110,16 +109,9 @@ int rmm_el3_ifc_get_platform_token(uintptr_t buf, size_t buflen,
 int rmm_el3_ifc_push_hes_request(uintptr_t buf, size_t buflen)
 {
 	struct smc_result smc_res;
-	unsigned long buffer_pa;
-	unsigned long offset = buf - rmm_shared_buffer_start_va;
-
-	assert((offset + buflen) <= rmm_el3_ifc_get_shared_buf_size());
-	assert((buf & ~PAGE_SIZE_MASK) == rmm_shared_buffer_start_va);
-
-	buffer_pa = (unsigned long)rmm_el3_ifc_get_shared_buf_pa() + offset;
 
 	monitor_call_with_res(SMC_RMM_HES_PUSH_ATTEST_REQ,
-			      buffer_pa,
+			      get_buffer_pa(buf, buflen),
 			      buflen,
 			      0UL,
 			      0UL, 0UL, 0UL, &smc_res);
@@ -138,16 +130,9 @@ int rmm_el3_ifc_pull_hes_response(uintptr_t buf, size_t buflen,
 					size_t *len)
 {
 	struct smc_result smc_res;
-	unsigned long buffer_pa;
-	unsigned long offset = buf - rmm_shared_buffer_start_va;
-
-	assert((offset + buflen) <= rmm_el3_ifc_get_shared_buf_size());
-	assert((buf & ~PAGE_SIZE_MASK) == rmm_shared_buffer_start_va);
-
-	buffer_pa = (unsigned long)rmm_el3_ifc_get_shared_buf_pa() + offset;
 
 	monitor_call_with_res(SMC_RMM_HES_PULL_ATTEST_RESP,
-			      buffer_pa,
+			      get_buffer_pa(buf, buflen),
 			      buflen,
 			      0UL, 0UL, 0UL, 0UL, &smc_res);
 
