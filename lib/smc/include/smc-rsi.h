07f07f8999ba662c086ced021b01b7b45e025b98
AlexeiFedorov <Alexei.Fedorov@arm.com>
2024-08-05 13:50:33 +0100

    feat(lib/smc): add RSI definitions
    This patch adds 'rpv' member to 'rsi_realm_config'
    structure and 'rsi_ripas' enumeration.
    
    Change-Id: Iad17115e3de82b96598f3d16d1c3b3583ed78112
    Signed-off-by: AlexeiFedorov <Alexei.Fedorov@arm.com>

--- lib/smc/include/smc-rsi.h
+++ lib/smc/include/smc-rsi.h
@@ -157,12 +157,25 @@
  */
 #define SET_MEMBER_RSI	SET_MEMBER
 
+/* Size of Realm Personalization Value */
+#ifndef CBMC
+#define RSI_RPV_SIZE		64
+#else
+/*
+ * Small RPV size so that RsiRealmConfig structure
+ * fits in the reduced sized granule defined for CBMC
+ */
+#define RSI_RPV_SIZE		1
+#endif
+
 /* RsiRealmConfig structure containing realm configuration */
 struct rsi_realm_config {
 	/* IPA width in bits */
 	SET_MEMBER_RSI(unsigned long ipa_width, 0, 8);		/* Offset 0 */
 	/* Hash algorithm */
-	SET_MEMBER_RSI(unsigned long algorithm, 8, 0x1000);	/* Offset 8 */
+	SET_MEMBER_RSI(unsigned long algorithm, 8, 0x200);	/* Offset 8 */
+	/* Realm Personalization Value */
+	SET_MEMBER_RSI(unsigned char rpv[RSI_RPV_SIZE], 0x200, 0x1000); /* Offset 0x200 */
 };
 
 #endif /* __ASSEMBLER__ */
@@ -172,6 +185,16 @@ struct rsi_realm_config {
  * ret0 == Status / error
  */
 #define SMC_RSI_REALM_CONFIG		SMC64_RSI_FID(U(0x6))
+
+#ifndef __ASSEMBLER__
+/* RsiRipas enumeration represents realm IPA state */
+enum rsi_ripas {
+	RSI_EMPTY = 0U,
+	RSI_RAM = 1U,
+	RSI_DESTROYED = 2U,
+	RSI_IO = 3U
+};
+#endif /* __ASSEMBLER__ */
 
 /*
  * arg1 == Base IPA address of target region
